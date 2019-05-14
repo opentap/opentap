425fc1425fcbef33e88a29ea0853f59b171509f4
Asger Iversen <asger_iversen@keysight.com>
2019-05-14 19:19:38 +0200

    Hide cache subcommand for now

--- Package/PackageActions/CacheAction.cs
+++ Package/PackageActions/CacheAction.cs
@@ -2,12 +2,14 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, you can obtain one at http://mozilla.org/MPL/2.0/.
+using System.ComponentModel;
 using System.Threading;
 using OpenTap.Cli;
 
 namespace OpenTap.Package
 {
     [Display("cache", "Operations related to the package cache.", "package" )]
+    [Browsable(false)]
     public class CacheAction : LockingPackageAction
     {
         [CommandLineArgument("clear", Description = "Clear the package cache.")]
