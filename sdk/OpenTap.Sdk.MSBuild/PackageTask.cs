ead14873f753bee12be5e58eaed88cf5a9adf488
Stefan Holst <stefan.holst@keysight.com>
2019-09-16 10:44:53 +0000

    Added the sdk generate tool to the sdk solution.

--- sdk/OpenTap.Sdk.MSBuild/PackageTask.cs
+++ sdk/OpenTap.Sdk.MSBuild/PackageTask.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿//            Copyright Keysight Technologies 2012-2019
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, you can obtain one at http://mozilla.org/MPL/2.0/.
+using System;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 using System.Diagnostics;
@@ -25,7 +29,7 @@ namespace Keysight.OpenTap.Sdk.MSBuild
     /// </summary>
     [LoadInSeparateAppDomain]
     [Serializable]
-    public class PackageTask : AppDomainIsolatedTask
+    public class PackageTask : Task
     {
         [Required]
         public string ConfFile { get; set; }
