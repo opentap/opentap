f648dbc382066dc686e0ea87e080bb1ab0b30b1f
Rolf Madsen <rolf_madsen@keysight.com>
2019-12-05 15:41:49 +0100

    5163 Package Dependency On Serializer Plugins
    - Added support for saving the serializer plugin type as a dependency as well.
    - Added a unit test ensring Package.Dependencies gets added.

--- Engine/TapSerialization.cs
+++ Engine/TapSerialization.cs
@@ -378,9 +378,12 @@ namespace OpenTap
             if(obj != null)
             {
                 type = TypeData.GetTypeData(obj);
+                NotifyTypeUsed(type);
             }
             if (Object.Equals(type, expectedType) == false && type != null)
                 elem.SetAttributeValue("type", type.Name);
+            else if (expectedType != null)
+                NotifyTypeUsed(type);
 
             foreach (var serializer in serializers)
             {
@@ -390,7 +393,10 @@ namespace OpenTap
                     if(serializer is ITapSerializerPlugin ser)
                     {
                         if (ser.Serialize(elem, obj, type))
+                        {
+                            NotifyTypeUsed(TypeData.GetTypeData(ser));
                             return true;
+                        }
                     }
                 }
                 finally
@@ -489,60 +495,18 @@ namespace OpenTap
             return serializer ?? GetCurrentSerializer();
         }
 
+        HashSet<ITypeData> registeredTypes = new HashSet<ITypeData>();
+
+        void NotifyTypeUsed(ITypeData type)
+        {
+            registeredTypes.Add(type);
+        }
+
+        /// <summary> Gets this TapSerializer instance has encountered until now. </summary>
+        public IEnumerable<ITypeData> GetUsedTypes() => registeredTypes;
+
         /// <summary> The path where the current file is being loaded from. This might be null in cases where it's being loaded from a stream.</summary>
         public string ReadPath { get; private set; }
     }
-    
-    internal static class StringCompressor
-    {
-        /// <summary>
-        /// Compresses the string.
-        /// </summary>
-        /// <param name="text">The text.</param>
-        /// <returns></returns>
-        public static string CompressString(string text)
-        {
-            byte[] buffer = Encoding.UTF8.GetBytes(text);
-            var memoryStream = new MemoryStream();
-            using (var gZipStream = new GZipStream(memoryStream, CompressionMode.Compress, true))
-            {
-                gZipStream.Write(buffer, 0, buffer.Length);
-            }
-
-            memoryStream.Position = 0;
-
-            var compressedData = new byte[memoryStream.Length];
-            memoryStream.Read(compressedData, 0, compressedData.Length);
-
-            var gZipBuffer = new byte[compressedData.Length + 4];
-            Buffer.BlockCopy(compressedData, 0, gZipBuffer, 4, compressedData.Length);
-            Buffer.BlockCopy(BitConverter.GetBytes(buffer.Length), 0, gZipBuffer, 0, 4);
-            return Convert.ToBase64String(gZipBuffer);
-        }
-
-        /// <summary>
-        /// Decompresses the string.
-        /// </summary>
-        /// <param name="compressedText">The compressed text.</param>
-        /// <returns></returns>
-        public static string DecompressString(string compressedText)
-        {
-            byte[] gZipBuffer = Convert.FromBase64String(compressedText);
-            using (var memoryStream = new MemoryStream())
-            {
-                int dataLength = BitConverter.ToInt32(gZipBuffer, 0);
-                memoryStream.Write(gZipBuffer, 4, gZipBuffer.Length - 4);
-
-                var buffer = new byte[dataLength];
-
-                memoryStream.Position = 0;
-                using (var gZipStream = new GZipStream(memoryStream, CompressionMode.Decompress))
-                {
-                    gZipStream.Read(buffer, 0, buffer.Length);
-                }
-
-                return Encoding.UTF8.GetString(buffer);
-            }
-        }
-    }
 }
+
---
+++ Package.UnitTests/SerializerDependencyTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using OpenTap.Plugins.BasicSteps;
+
+namespace OpenTap.Package.UnitTests
+{
+    [TestFixture]
+    public class SerializerDependencyTest
+    {
+        [Test]
+        public void VerifyPackageDependencies()
+        {
+            var tp = new TestPlan();
+            tp.ChildTestSteps.Add(new DelayStep());
+            var serializer = new TapSerializer();
+            
+            var str = serializer.SerializeToString(tp);
+            Assert.IsTrue(str.Contains("<Package.Dependencies>"));
+            Assert.IsTrue(serializer.GetUsedTypes().Contains(TypeData.FromType(typeof(DelayStep))));
+        }
+    }
+}
