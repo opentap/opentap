808ac4ebf3c98484a7a9de284a3b77eeeb3d81e4
allarsen <alexander.larsen@keysight.com>
2020-11-23 09:16:39 +0100

    ask about including dependencies if 'Interactive' is true

--- Package/PackageActions/Download.cs
+++ Package/PackageActions/Download.cs
@@ -90,7 +90,9 @@ namespace OpenTap.Package
             else
                 repositories.AddRange(Repository.Select(s => PackageRepositoryHelpers.DetermineRepositoryType(s)));
 
-            List<PackageDef> PackagesToDownload = PackageActionHelpers.GatherPackagesAndDependencyDefs(destinationInstallation, PackageReferences, Packages, Version, Architecture, OS, repositories, ForceInstall, InstallDependencies, false, false);
+            List<PackageDef> PackagesToDownload = PackageActionHelpers.GatherPackagesAndDependencyDefs(
+                destinationInstallation, PackageReferences, Packages, Version, Architecture, OS, repositories,
+                ForceInstall, InstallDependencies, false, false);
 
             if (PackagesToDownload == null)
                 return 2;
