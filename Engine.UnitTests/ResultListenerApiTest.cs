c3c1aad61953f68e8c519819b4290ef270dcd1a2
Rolf Madsen <rolf_madsen@keysight.com>
2021-02-01 11:10:23 +0100

    548 HybridStream bug: Fix
    Fixed issue by adding FileShare.ReadWrite to the HybridStream file stream.

--- Engine.UnitTests/ResultListenerApiTest.cs
+++ Engine.UnitTests/ResultListenerApiTest.cs
@@ -16,11 +16,15 @@ namespace OpenTap.UnitTests
                 var rl = new RecordAllResultListener();
                 var plan = new TestPlan();
                 var log = new LogStep();
+                
+                // force the hybrid log stream to save in a file by
+                // writing ~10MB log data.
                 log.LogMessage = new string('A', 100000);
                 var repeat = new RepeatStep() {Count = 100};
                 repeat.ChildTestSteps.Add(log);
                 plan.ChildTestSteps.Add(repeat);
                 var run = plan.Execute(new IResultListener[] {rl});
+                Log.Flush();
                 Assert.AreEqual(Verdict.NotSet, run.Verdict);
                 Assert.IsTrue(logListener.ErrorMessage.Count == 0);
             }
