2b7b7bcf278cd0bd6d71b4c5ab0e3c3a44daeab3
Stefan Holst <stefan.holst@keysight.com>
2021-03-12 13:43:49 +0000

    Fixed issue with opentap not being able to update it self. It was caused by a call to the constructor of `ConsoleTraceListener`. Instead moved the code invoked through the constructor out to be called separately.

--- Engine/ConsoleTraceListener.cs
+++ Engine/ConsoleTraceListener.cs
@@ -60,7 +60,7 @@ namespace OpenTap
                 }
                 catch (Exception ex)
                 {
-                    InternalTraceEvent("Console", LogEventType.Debug, 0, $"Error while enabeling ANSI colors: {ex.Message}", 0);
+                    InternalTraceEvent("Console", LogEventType.Debug, 0, $"Error while enabling ANSI colors: {ex.Message}", 0);
                 }
             }
             else
@@ -193,47 +193,6 @@ namespace OpenTap
                     Console.ForegroundColor = currentColor;
                 }
             }
-        }
-    }
-
-    class AnsiColorCodeFix
-    {
-        private const int STD_OUTPUT_HANDLE = -11;
-        private const uint ENABLE_VIRTUAL_TERMINAL_PROCESSING = 0x0004;
-        private const uint DISABLE_NEWLINE_AUTO_RETURN = 0x0008;
-
-        [DllImport("kernel32.dll")]
-        private static extern bool GetConsoleMode(IntPtr hConsoleHandle, out uint lpMode);
-
-        [DllImport("kernel32.dll")]
-        private static extern bool SetConsoleMode(IntPtr hConsoleHandle, uint dwMode);
-
-        [DllImport("kernel32.dll", SetLastError = true)]
-        private static extern IntPtr GetStdHandle(int nStdHandle);
-
-        [DllImport("kernel32.dll")]
-        public static extern uint GetLastError();
-
-        /// <summary>
-        /// This should work for win10 version 1511 or later
-        /// </summary>
-        public static bool TryEnableForWin10()
-        {
-            var iStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-            if (!GetConsoleMode(iStdOut, out uint outConsoleMode))
-            {
-                // this happens when std out is redirected e.g. when running isolated. 
-                // We rely on the parent process to have completed this fix
-                return false;
-            }
-
-            outConsoleMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING | DISABLE_NEWLINE_AUTO_RETURN;
-            if (!SetConsoleMode(iStdOut, outConsoleMode))
-            {
-                return false;
-            }
-
-            return true;
         }
     }
 }
