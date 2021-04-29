ee7528b7507a11ab8e4b896603db555695ab9297
Rolf Madsen <rolf_madsen@keysight.com>
2021-04-29 16:28:28 +0200

    639 TapRun Metadata bug: Added test

--- Engine.UnitTests/ResultTest.cs
+++ Engine.UnitTests/ResultTest.cs
@@ -1,5 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using NUnit.Framework;
+using OpenTap.Engine.UnitTests;
+using OpenTap.Plugins.BasicSteps;
 
 namespace OpenTap.UnitTests
 {
@@ -40,5 +46,52 @@ namespace OpenTap.UnitTests
             Assert.AreEqual(1.0, columnA.Data.GetValue(0));
             Assert.AreEqual(2.0, columnB.Data.GetValue(0));
         }
+
+        [Test]
+        public void TestSimpleResults3()
+        {
+            var step = new SimpleResultTest();
+            var seq = new SequenceStep();
+            var plan = new TestPlan();
+            plan.Steps.Add(seq);
+            seq.ChildTestSteps.Add(step);
+            
+            var rl = new SimpleResultTest2();
+            var metadata = new ResultParameter("", "MetaData1", "Value1", new MetaDataAttribute());
+            Assert.IsTrue(metadata.IsMetaData);
+            var run = plan.Execute(new[] {rl}, new[] {metadata});
+            Assert.IsTrue(run.Parameters.Find("MetaData1").IsMetaData);
+
+        }
+
+        class SimpleResultTest2 : ResultListener
+        {
+            
+            Dictionary<Guid, TestRun> runs = new Dictionary<Guid, TestRun>();
+            public override void OnTestPlanRunStart(TestPlanRun planRun) => runs.Add(planRun.Id, planRun);
+            
+            public override void OnTestPlanRunCompleted(TestPlanRun planRun, Stream logStream) => runs.Remove(planRun.Id);
+            public override void OnTestStepRunStart(TestStepRun stepRun) => runs.Add(stepRun.Id, stepRun);
+            public override void OnTestStepRunCompleted(TestStepRun stepRun) => runs.Remove(stepRun.Id);
+
+            public override void OnResultPublished(Guid stepRunId, ResultTable result)
+            {
+                base.OnResultPublished(stepRunId, result);
+                ResultParameters parameterList = new ResultParameters();
+                Guid runid = stepRunId;
+                while (runs.TryGetValue(runid, out TestRun subRun))
+                {
+                    foreach(var subparameter in subRun.Parameters.Where(parameter => parameter.IsMetaData))
+                        if (parameterList.Find(subparameter.Name) == null)
+                        {
+                            parameterList.Add(subparameter);
+                        }
+                    if (subRun is TestStepRun run)
+                        runid = run.Parent;
+                    else break;
+                }
+            }
+        }
+        
     }
 }
