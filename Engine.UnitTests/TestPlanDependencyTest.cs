8e7bdbece997d992cd1f52cfdcec7a4838400114
allarsen <alexander.larsen@keysight.com>
2021-06-28 09:46:37 +0200

    move new test to new file

--- Engine.UnitTests/TestPlanDependencyTest.cs
+++ Engine.UnitTests/TestPlanDependencyTest.cs
@@ -36,33 +36,6 @@ namespace OpenTap.UnitTests
     }
 
     [TestFixture]
-    public class ImageAnnotationTest
-    {
-        [Test]
-        public void AnnotationTest()
-        {
-            var step = new MyImageUsingTestStep();
-            var a = AnnotationCollection.Annotate(step);
-            var mem = a.GetMember(nameof(step.Image));
-            var img = mem.Get<IImageAnnotation>();
-
-            StringAssert.AreEqualIgnoringCase(img.ImageSource, TestPlanDependencyTest.ImageReference);
-            StringAssert.AreEqualIgnoringCase(img.Description, TestPlanDependencyTest.ImageDescription);
-
-            step.Image.Description = "test1";
-            mem.Read(step.Image);
-            
-
-            StringAssert.AreEqualIgnoringCase(img.Description, "test1");
-
-            (img as ImageAnnotation).Description = "test2";
-            mem.Write(step.Image);
-
-            Assert.AreEqual(step.Image.Description, "test2");
-        }
-    }
-
-    [TestFixture]
     public class TestPlanDependencyTest
     {
         private string[] _files => new[] {ReferencedFile, ReferencedFile2, ImageReference};
