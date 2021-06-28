8de6effa78a06b903cc2689e9c4422c88e53e2db
allarsen <alexander.larsen@keysight.com>
2021-06-28 09:32:20 +0200

    Add IImageAnnotation types and extend unittests for images

--- Engine/OpenTapImage.cs
+++ Engine/OpenTapImage.cs
@@ -1,14 +1,77 @@
 namespace OpenTap
 {
+    /// <summary> Marks that the annotated object is an image. </summary>
+    public interface IImageAnnotation : IAnnotation
+    {
+        /// <summary>
+        /// Specifies the path to the image
+        /// </summary>
+        string ImageSource { get; }
+
+        /// <summary>
+        /// Specifies a description of the image
+        /// </summary>
+        string Description { get; }
+
+    }
+
+    class ImageAnnotation : IImageAnnotation, IOwnedAnnotation
+    {
+        public ImageAnnotation(string imageSource, string description)
+        {
+            ImageSource = imageSource;
+            Description = description;
+        }
+
+        public string ImageSource { get; set; }
+
+        public string Description { get; set; }
+
+        public void Read(object source)
+        {
+            if (source is IOpenTapImage img)
+            {
+                ImageSource = img.ImageSource;
+                Description = img.Description;
+            }
+        }
+
+        public void Write(object source)
+        {
+            if (source is IOpenTapImage img)
+            {
+                img.ImageSource = ImageSource;
+                img.Description = Description;
+            }
+        }
+    }
+
     /// <summary>
     /// Represents an image resource
     /// </summary>
-    public class OpenTapImage
+    public interface IOpenTapImage
+    {
+        /// <summary>
+        /// Specifies the path to the image
+        /// </summary>
+        string ImageSource { get; set; }
+
+        /// <summary>
+        /// Specifies a description of the image
+        /// </summary>
+        string Description { get; set; }
+    }
+
+    /// <summary>
+    /// Represents an image resource
+    /// </summary>
+    public class OpenTapImage : IOpenTapImage
     {
         /// <summary>
         /// Specifies the path to the image
         /// </summary>
         [FilePath]
+        [Display("Image File")]
         public string ImageSource { get; set; }
 
         /// <summary>
