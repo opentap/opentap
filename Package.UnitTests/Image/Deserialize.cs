45ed40cbbe18b236a470261881f9ba79ae67772b
DENNIS RASMUSSEN <dennis.rasmussen@keysight.com>
2021-08-28 12:06:59 +0200

    Implemented the Image Concept with xml and json deserialization and unittests

---
+++ Package.UnitTests/Image/Deserialize.cs
@@ -0,0 +1,104 @@
+﻿using NUnit.Framework;
+using OpenTap.Image;
+using OpenTap.Package;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenTap.Image.Tests
+{
+    [TestFixture]
+    public class Deserialize
+    {
+        [Test]
+        public void TestJsonImage()
+        {
+
+            string value = @"
+            {
+                ""Packages"": [
+                    {
+                        ""Name"": ""Demonstration""
+                    },
+                    {
+                        ""Name"": ""Yardstick"",
+                        ""Version"": ""beta"",
+                        ""OS"": ""Windows"",
+                        ""Architecture"": ""AnyCPU""
+                    },
+                    {
+                        ""Name"": ""OpenTAP"",
+                        ""Version"": ""9.15.2""
+                    },
+                    {
+                        ""Name"": ""REST-API"",
+                        ""Version"": ""beta""
+                    }
+                ],
+                ""Repositories"": [
+                    ""packages.opentap.io"",
+                    ""packages.opentap.keysight.com""
+                ]
+            }";
+
+            var specifier = ImageSpecifier.FromString(value);
+            Assert.True(specifier.Packages.Count == 4);
+            Assert.True(specifier.Packages[0].Name == "Demonstration");
+            Assert.True(specifier.Packages[1].Name == "Yardstick");
+            Assert.True(specifier.Packages[2].Name == "OpenTAP");
+            Assert.True(specifier.Packages[3].Name == "REST-API");
+
+            Assert.True(specifier.Packages[1].Version == VersionSpecifier.Parse("beta"));
+            Assert.True(specifier.Packages[1].Architecture == CpuArchitecture.AnyCPU);
+            Assert.True(specifier.Packages[1].OS == "Windows");
+
+            Assert.True(specifier.Repositories.Count == 2);
+            Assert.True(specifier.Repositories[0] == "packages.opentap.io");
+            Assert.True(specifier.Repositories[1] == "packages.opentap.keysight.com");
+
+        }
+
+        [Test]
+        public void TestXmlImage()
+        {
+
+            string value = @"<?xml version=""1.0""?>
+<Image xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"">
+  <Packages>
+    <PackageSpecifier Name=""Editor"" Version=""9.9.1+ca3d0108"" OS=""Windows"" Architecture=""x64"" />
+    <PackageSpecifier Name=""TUI"" Version=""any"" />
+    <PackageSpecifier Name=""Demonstration"" Version=""9.0.3+cb113229"" OS=""Windows,Linux"" Architecture=""AnyCPU"" />
+  </Packages>
+  <Repositories>
+    <string>https://packages.opentap.io</string>
+    <string>C:\git\installercreate\bin\Debug</string>
+  </Repositories>
+</Image>
+";
+
+            var specifier = ImageSpecifier.FromString(value);
+
+            Assert.True(specifier.Packages.Count == 3);
+            Assert.True(specifier.Packages[0].Name == "Editor");
+            Assert.True(specifier.Packages[1].Name == "TUI");
+            Assert.True(specifier.Packages[2].Name == "Demonstration");
+
+            Assert.True(specifier.Packages[0].Version == VersionSpecifier.Parse("9.9.1+ca3d0108"));
+            Assert.True(specifier.Packages[0].Architecture == CpuArchitecture.x64);
+            Assert.True(specifier.Packages[0].OS == "Windows");
+
+            Assert.True(specifier.Packages[1].Version == VersionSpecifier.Parse("any"));
+
+            Assert.True(specifier.Packages[2].Version == VersionSpecifier.Parse("9.0.3+cb113229"));
+            Assert.True(specifier.Packages[2].Architecture == CpuArchitecture.AnyCPU);
+            Assert.True(specifier.Packages[2].OS == "Windows,Linux");
+
+            Assert.True(specifier.Repositories.Count == 2);
+            Assert.True(specifier.Repositories[0] == "https://packages.opentap.io");
+            Assert.True(specifier.Repositories[1] == @"C:\git\installercreate\bin\Debug");
+        }
+    }
+}
---
+++ Package/Image/ImageHelper.cs
@@ -0,0 +1,39 @@
+﻿using OpenTap.Package;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace OpenTap.Package
+{
+    internal static class ImageHelper
+    {
+        internal static ImageSpecifier GetImageFromString(string value)
+        {
+            if (value.IsXml())
+            {
+                return ImageXmlSerializer.DeserializeImageSpecifier(value);
+            }
+            else if (value.IsJson())
+            {
+                return ImageJsonSerializer.DeserializeImageSpecifier(value);
+
+            }
+            throw new InvalidOperationException("Value could not be parsed as JSON or XML");
+        }
+
+        static bool IsJson(this string jsonData)
+        {
+            return jsonData.Trim().Substring(0, 1).IndexOfAny(new[] { '[', '{' }) == 0;
+        }
+
+        static bool IsXml(this string xmlData)
+        {
+            return xmlData.Trim().Substring(0, 1).IndexOfAny(new[] { '<' }) == 0;
+        }
+    }
+
+}
