30d50419b06270d4a1bbb29e67ceb849e1eeab8a
allarsen <alexander.larsen@keysight.com>
2021-09-09 14:53:40 +0200

    Change visibility of new interfaces and  cache provider instances. Remove GetPictureName

--- Engine.UnitTests/DefaultPictureDataProviderTest.cs
+++ Engine.UnitTests/DefaultPictureDataProviderTest.cs
@@ -11,26 +11,19 @@ namespace OpenTap.UnitTests
         public class TestPictureProvider : IPictureDataProvider
         {
             public const string Format = "Test Format";
-            public const string Name = "Test Format";
             // Go after DefaultPictureDataProvider
-            public double Order =>
-                ((IPictureDataProvider) TypeData.FromType(typeof(DefaultPictureDataProvider)).CreateInstance()).Order +
-                1;
-            public Task<Stream> GetStream(IPicture picture)
+            public double Order => 999;
+
+            Task<Stream> IPictureDataProvider.GetStream(IPicture picture)
             {
                 var stream = new MemoryStream();
                 stream.Write(new byte[] {1,2,3,4,5,6,7,8,9,10}, 0, 10);
                 return Task.FromResult<Stream>(stream);
             }
 
-            public Task<string> GetPictureFormat(IPicture picture)
+            public Task<string> GetFormat(IPicture picture)
             {
                 return Task.FromResult(Format);
-            }
-
-            public Task<string> GetPictureName(IPicture picture)
-            {
-                return Task.FromResult(Name);
             }
         }
 
@@ -38,19 +31,19 @@ namespace OpenTap.UnitTests
         [Test]
         public async Task TestFilePicture()
         {
-            var source = Path.Combine(ExecutorClient.ExeDir, "Resources/TestImg.png");
+            var dir = Path.GetDirectoryName(typeof(DefaultPictureDataProviderTest).Assembly.Location);
+            var source = Path.Combine(dir, "Resources/TestImg.png");
 
             var pic = new Picture() {Source = source, Description = "Test Picture"};
 
             {
                 // Test file name and file type
-                Assert.AreEqual("TestImg", await PictureDataProvider.GetPictureName(pic));
-                Assert.AreEqual("png", await PictureDataProvider.GetPictureFormat(pic));
+                Assert.AreEqual("png", await pic.GetFormat());
             }
 
             var expectedBytes = File.ReadAllBytes(source);
 
-            using (var picStream = await PictureDataProvider.GetStream(pic))
+            using (var picStream = await pic.GetStream())
             {
                 using (var memoryStream = new MemoryStream())
                 {
@@ -68,8 +61,7 @@ namespace OpenTap.UnitTests
             var pic = new Picture() {Source = source, Description = "Test Picture"};
 
             // Ensure filenames and types are correct
-            Assert.AreEqual("package.a7440fd6", await PictureDataProvider.GetPictureName(pic));
-            Assert.AreEqual("png", await PictureDataProvider.GetPictureFormat(pic));
+            Assert.AreEqual("png", await pic.GetFormat());
 
             byte[] expectedBytes;
             {
@@ -80,7 +72,7 @@ namespace OpenTap.UnitTests
                 expectedBytes = data.Content.ReadAsByteArrayAsync().Result;
             }
 
-            using (var picStream = await PictureDataProvider.GetStream(pic))
+            using (var picStream = await pic.GetStream())
             {
                 using (var memoryStream = new MemoryStream())
                 {
@@ -96,10 +88,9 @@ namespace OpenTap.UnitTests
             var source = "Source which does not exist";
             var pic = new Picture() {Source = source, Description = "Non-existent"};
             
-            Assert.AreEqual(TestPictureProvider.Format, await PictureDataProvider.GetPictureFormat(pic));
-            Assert.AreEqual(TestPictureProvider.Name, await PictureDataProvider.GetPictureName(pic));
+            Assert.AreEqual(TestPictureProvider.Format, await pic.GetFormat());
 
-            var bytes = await PictureDataProvider.GetStream(pic) as MemoryStream;
+            var bytes = await pic.GetStream() as MemoryStream;
             CollectionAssert.AreEqual(new byte[]{1,2,3,4,5,6,7,8,9,10}, bytes.ToArray());
 
         }
--- Engine/IPictureDataProvider.cs
+++ Engine/IPictureDataProvider.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -9,9 +10,9 @@ using System.Threading.Tasks;
 namespace OpenTap
 {
     /// <summary>
-    /// A mechanism for retrieving <see cref="IPicture"/> data with <see cref="PictureDataProvider"/>
+    /// A mechanism for retrieving <see cref="IPicture"/> data with <see cref="PictureDataExtensions"/>
     /// </summary>
-    public interface IPictureDataProvider
+    internal interface IPictureDataProvider
     {
         /// <summary>
         /// The order in which IPictureDataProviders will be tested. Lowers numbers go first
@@ -28,13 +29,7 @@ namespace OpenTap
         /// </summary>
         /// <param name="picture"></param>
         /// <returns></returns>
-        Task<string> GetPictureFormat(IPicture picture);
-        /// <summary>
-        /// Get the name of the picture
-        /// </summary>
-        /// <param name="picture"></param>
-        /// <returns></returns>
-        Task<string> GetPictureName(IPicture picture);
+        Task<string> GetFormat(IPicture picture);
     }
     
     /// <summary>
@@ -44,9 +39,30 @@ namespace OpenTap
     {
         public double Order => 10;
 
+        /// <summary>
+        /// Relative URIs are poorly supported in dotnet core. Ensure we only use absolute URIs by normalizing path strings to absolute paths.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        private string normalizeSource(string source)
+        {
+            try
+            {
+                if (File.Exists(source))
+                    return Path.GetFullPath(source);
+            }
+            catch
+            {
+                // this is fine -- source is not a file
+            }
+
+            return source;
+        }
+
         public Task<Stream> GetStream(IPicture picture)
         {
-            if (Uri.TryCreate(picture.Source, UriKind.RelativeOrAbsolute, out var uri))
+            var source = normalizeSource(picture.Source);
+            if (Uri.TryCreate(source, UriKind.Absolute, out var uri))
             {
                 var req = WebRequest.Create(uri);
                 return req.GetResponseAsync().ContinueWith(t => t.Result.GetResponseStream());
@@ -55,25 +71,14 @@ namespace OpenTap
             return null;
         }
 
-        public Task<string> GetPictureFormat(IPicture picture)
+        public Task<string> GetFormat(IPicture picture)
         {
-            if (Uri.TryCreate(picture.Source, UriKind.RelativeOrAbsolute, out var uri))
+            var source = normalizeSource(picture.Source);
+            if (Uri.TryCreate(source, UriKind.Absolute, out var uri))
             {
                 var name = uri.Segments.LastOrDefault();
                 if (string.IsNullOrWhiteSpace(name) == false)
                     return Task.FromResult(Path.GetExtension(name).Substring(1));
-            }
-
-            return null;
-        }
-
-        public Task<string> GetPictureName(IPicture picture)
-        {
-            if (Uri.TryCreate(picture.Source, UriKind.RelativeOrAbsolute, out var uri))
-            {
-                var name = uri.Segments.LastOrDefault();
-                if (string.IsNullOrWhiteSpace(name) == false)
-                    return Task.FromResult(Path.GetFileNameWithoutExtension(name));
             }
 
             return null;
@@ -84,16 +89,28 @@ namespace OpenTap
     /// <summary>
     /// Provide <see cref="IPicture"/> data from <see cref="IPictureDataProvider"/> implementations.
     /// </summary>
-    public static class PictureDataProvider
+    public static class PictureDataExtensions
     {
+        private static IPictureDataProvider[] cache = Array.Empty<IPictureDataProvider>();
+        private static ISet<ITypeData> cacheKey;
+
         private static IEnumerable<IPictureDataProvider> GetProviders()
         {
-            return TypeData.GetDerivedTypes<IPictureDataProvider>().Where(td => td.CanCreateInstance)
-                .Select(td => td.CreateInstance()).OfType<IPictureDataProvider>().OrderBy(p => p.Order);
-        }
-        
+            var types = TypeData.GetDerivedTypes<IPictureDataProvider>()
+                .Where(td => td.CanCreateInstance).ToImmutableHashSet();
+            
+            if (cacheKey == null || types.SetEquals(cacheKey) == false)
+            {
+                cache = types.Select(td => td.CreateInstance()).OfType<IPictureDataProvider>().OrderBy(p => p.Order)
+                    .ToArray();
+                cacheKey = types;
+            }
 
-        private static TraceSource log = Log.CreateSource(nameof(PictureDataProvider));
+            return cache;
+        }
+
+
+        private static TraceSource log = Log.CreateSource(nameof(PictureDataExtensions));
         private static async Task<T> GetFirst<T>(IPicture picture, Func<IPicture, IPictureDataProvider, Task<T>> func)
         {
             foreach (var provider in GetProviders())
@@ -119,21 +136,13 @@ namespace OpenTap
         /// Get a stream of the picture from the first <see cref="IPictureDataProvider"/> which returns a non-null.
         /// </summary>
         /// <param name="picture"></param>
-        public static Task<Stream> GetStream(IPicture picture) =>
+        public static Task<Stream> GetStream(this IPicture picture) =>
             GetFirst(picture, (pic, provider) => provider.GetStream(pic)); 
 
         /// <summary>
         /// Get the format of the picture from the first <see cref="IPictureDataProvider"/> which returns a non-null.
         /// </summary>
         /// <param name="picture"></param>
-        public static Task<string> GetPictureFormat(IPicture picture) => GetFirst(picture, (pic, provider) => provider.GetPictureFormat(pic));
-
-        /// <summary>
-        /// Get the name of the picture from the first <see cref="IPictureDataProvider"/> which returns a non-null.
-        /// </summary>
-        /// <param name="picture"></param>
-        /// <returns></returns>
-
-        public static Task<string> GetPictureName(IPicture picture) => GetFirst(picture, (pic, provider) => provider.GetPictureName(pic));
+        public static Task<string> GetFormat(this IPicture picture) => GetFirst(picture, (pic, provider) => provider.GetFormat(pic));
     }
 }
