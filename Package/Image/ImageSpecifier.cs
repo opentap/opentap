b114c9abd21f95fd7e54007f2a6ccf55ab3aa220
Stefan Holst <stefan.holst@keysight.com>
2021-10-13 23:04:36 +0000

    Added check to ensure image install use the same/correct os and architectures for all packages.

--- Package/Image/ImageSpecifier.cs
+++ Package/Image/ImageSpecifier.cs
@@ -33,22 +33,36 @@ namespace OpenTap.Package
             List<Exception> exceptions = new List<Exception>();
             List<IPackageRepository> repositories = Repositories.Select(s => PackageRepositoryHelpers.DetermineRepositoryType(s)).ToList();
             List<PackageDef> gatheredPackages = new List<PackageDef>();
-            foreach (var packageReference in Packages)
+
+            // Check if all package only specify compatible oss
+            var oss = Packages.Select(p => p.OS).Distinct().Where(o => string.IsNullOrEmpty(o) == false && o.Contains(",") == false).ToList();
+            if (oss.Count > 1)
+                throw new InvalidOperationException("Unable to resolve image. Image specifies multiple operating systems.");
+            var os = oss.FirstOrDefault() ?? OperatingSystem.Current.Name;
+            
+            // Check if all package only specify compatible architectures
+            var archs = Packages.Select(p => p.Architecture).Distinct()
+                                                .Where(a => a != CpuArchitecture.Unspecified && a != CpuArchitecture.AnyCPU).ToList();
+            if (archs.Count > 1)
+                throw new InvalidOperationException("Unable to resolve image. Image specifies multiple architectures.");
+            var arch = archs.Any() ? archs[0] : ArchitectureHelper.GuessBaseArchitecture;
+            
+            foreach (var specifier in Packages)
             {
                 if (cancellationToken.IsCancellationRequested)
                     throw new OperationCanceledException("Resolve operation cancelled by user");
                 try
                 {
-                    PackageDef package = PackageActionHelpers.FindPackage(packageReference, new List<PackageDef>(), repositories);
+                    PackageDef package = PackageActionHelpers.FindPackage(new PackageSpecifier(specifier.Name, specifier.Version, arch, os), new List<PackageDef>(), repositories);
                     gatheredPackages.Add(package);
                 }
                 catch (Exception)
                 {
-                    PackageDef package = OnResolve?.Invoke(new ImageSpecifierResolveArgs(this, packageReference));
+                    PackageDef package = OnResolve?.Invoke(new ImageSpecifierResolveArgs(this, specifier));
                     if (package != null)
                         gatheredPackages.Add(package);
                     else
-                        exceptions.Add(new InvalidOperationException($"Unable to resolve package '{packageReference.Name}'"));
+                        exceptions.Add(new InvalidOperationException($"Unable to resolve package '{specifier.Name}'"));
                 }
             }
             if (cancellationToken.IsCancellationRequested)
