72008ebe97aa27aba6e2578487bee0401ff65833
Asger Iversen <asger_iversen@keysight.com>
2022-01-25 22:28:00 +0100

    Added xml comments to new ISettingReferenceIconAnnotation

--- Engine/Annotations/ISettingReferenceIconAnnotation.cs
+++ Engine/Annotations/ISettingReferenceIconAnnotation.cs
@@ -4,9 +4,16 @@ using System.Text;
 
 namespace OpenTap
 {
+    /// <summary> Specialization of <see cref="IIconAnnotation"/> that represents a reference to a setting on another TestStep.</summary>
     public interface ISettingReferenceIconAnnotation : IIconAnnotation
     {
+        /// <summary>
+        /// The TestStep that holds the setting beeing referenced
+        /// </summary>
         Guid TestStepReference { get; }
+        /// <summary>
+        /// The name of the setting being referenced
+        /// </summary>
         string MemberName { get; }
     }
 }
