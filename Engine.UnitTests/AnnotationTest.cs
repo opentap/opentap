525be6a008bc05058fe2f8c4ac3a272669a85e0f
Rolf Madsen <rolf_madsen@keysight.com>
2022-02-03 13:43:42 +0100

    264 Support Overlapping Enum Values: Test
    Added a unit test to show the issue.

--- Engine.UnitTests/AnnotationTest.cs
+++ Engine.UnitTests/AnnotationTest.cs
@@ -1671,6 +1671,41 @@ namespace OpenTap.UnitTests
             annotation.Write();
             Assert.IsTrue(obj.Values.SequenceEqual(new int[] {1, 2, 3, 4}));
         }
-        
+
+        public enum Overlapping
+        {
+            A = 0,
+            X = 1,
+            Z = 2, // there is one constraint, which is that the selected name must be the first one.
+            [Obsolete]
+            [Browsable(false)]
+            Y = 2,
+            [Browsable(false)]
+            [Obsolete]
+            Y2 = 2,
+            W = 3
+        }
+
+        class ClassWithOverlapping
+        {
+            public Overlapping Overlapping { get; set; } = Overlapping.X;
+        }
+        [Test]
+        public void OverlappingEnumTest()
+        {
+            var o = new ClassWithOverlapping();
+            var a = AnnotationCollection.Annotate(o);
+            var a2 = a.GetMember(nameof(o.Overlapping));
+            var available = a2.Get<IAvailableValuesAnnotation>().AvailableValues.Cast<Overlapping>();
+            Assert.IsTrue(available.SequenceEqual(new[] {Overlapping.A, Overlapping.X, Overlapping.Z, Overlapping.W}));
+
+            var a3 = a2.Get<IAvailableValuesAnnotationProxy>();
+            var strValues = a3.AvailableValues.Select(x => x.Get<IStringReadOnlyValueAnnotation>().Value).ToArray();
+            Assert.IsTrue(strValues.SequenceEqual(new []{"A", "X", "Z", "W"}));
+            a3.SelectedValue = a3.AvailableValues.Skip(2).FirstOrDefault();
+            a.Write();
+            a.Read();
+            Assert.AreEqual(Overlapping.Z, o.Overlapping);
+        }
     }
 }
