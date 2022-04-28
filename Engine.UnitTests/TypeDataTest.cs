78cac506f2f13cad7885bf1b0801a4d0430e33f9
Frederik Andreasen <frederik.andreasen@keysight.com>
2022-04-28 12:31:48 +0200

    TypeData Unittest Unit test for setting valutetype properties on a typedata member to null. This shouldn't fail as PropertyInfo.SetValue(xx, null) also doesn't fail.

---
+++ Engine.UnitTests/TypeDataTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenTap.UnitTests
+{
+    public class TypeDataTest
+    {
+        public class IntObject
+        {
+            public int SomeInt { get; set; }
+        }
+
+        [Test]
+        public void SettingPropertyToInvalidType()
+        {
+            var obj = new IntObject();
+            var typeData = TypeData.GetTypeData(obj);
+            var memberData = typeData.GetMember(nameof(IntObject.SomeInt));
+            Assert.DoesNotThrow(() => memberData.SetValue(obj, 123));
+            // PropertyInfo.SetValue(xx, null) does not throw an exception for value types.
+            // https://docs.microsoft.com/de-de/dotnet/api/system.reflection.propertyinfo.setvalue
+            Assert.DoesNotThrow(() => memberData.SetValue(obj, null));
+        }
+    }
+}
