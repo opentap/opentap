a0f981c29d9e169aa19941feeb0e9c2d5c579044
allarsen <alexander.larsen@keysight.com>
2022-05-02 13:39:56 +0200

    Move responsibility of ensuring directory existence.

--- Package/PackageInstallHelpers/FileLocks.cs
+++ Package/PackageInstallHelpers/FileLocks.cs
@@ -19,7 +19,6 @@ namespace OpenTap.Package.PackageInstallHelpers
     {
         public static IFileLock Create(string file)
         {
-            FileSystemHelper.EnsureDirectory(file);
             if (OperatingSystem.Current == OperatingSystem.Windows) return new Win32FileLock(file);
             if (OperatingSystem.Current == OperatingSystem.MacOS) return new MacOSFileLock(file);
             return new PosixFileLock(file);
