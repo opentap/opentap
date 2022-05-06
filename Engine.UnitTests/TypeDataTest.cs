9149fea7b56d40985dbddb346ef8bf7831674de0
Rolf Madsen <rolf_madsen@keysight.com>
2022-05-06 13:13:55 +0200

    537 TypeDataUnbrowsableBug: Unit test
    Added a unit test which shows that TypeData.FromType creates an unbrowsable type in this case. Also added a test that IsBrowsable should be false if Browsable(False) is used.

--- Engine.UnitTests/TypeDataTest.cs
+++ Engine.UnitTests/TypeDataTest.cs
@@ -1,7 +1,10 @@
 ﻿using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -24,6 +27,36 @@ namespace OpenTap.UnitTests
             // PropertyInfo.SetValue(xx, null) does not throw an exception for value types.
             // https://docs.microsoft.com/de-de/dotnet/api/system.reflection.propertyinfo.setvalue
             Assert.DoesNotThrow(() => memberData.SetValue(obj, null));
+        }
+
+        [Test]
+        public void DynamicTypeBrowsableFalse()
+        {
+            // Create an empty dynamic type which is public.
+            var domain = AppDomain.CurrentDomain;
+            var assemblyName = "TestAssembly";
+            var assemblyBuilder = domain.DefineDynamicAssembly(new AssemblyName(assemblyName), AssemblyBuilderAccess.Run);
+            var moduleBuilder = assemblyBuilder.DefineDynamicModule("TestModule");
+            TypeBuilder typeBuilder = moduleBuilder.DefineType("TestType",
+                TypeAttributes.Public | TypeAttributes.Class,
+                typeof(object),Array.Empty<Type>());
+            var type = typeBuilder.CreateType();
+            var td = TypeData.FromType(type);
+            
+            // A problem caused these kinds of type data to have IsBrowsable set to false, even though it does not 
+            // even have a BrowsableAttribute assigned.
+            Assert.IsTrue(td.IsBrowsable);
+            
+            TypeBuilder typeBuilder2 = moduleBuilder.DefineType("TestType2",
+                TypeAttributes.Public | TypeAttributes.Class,
+                typeof(object),Array.Empty<Type>());
+            CustomAttributeBuilder attrBuilder = new CustomAttributeBuilder(
+                typeof(BrowsableAttribute).GetConstructor(new Type[] { typeof(bool) }), new object[] { false });
+            typeBuilder2.SetCustomAttribute(attrBuilder);
+
+            var td2 = TypeData.FromType(typeBuilder2.CreateType());
+            Assert.IsFalse(td2.IsBrowsable);
+
         }
     }
 }
