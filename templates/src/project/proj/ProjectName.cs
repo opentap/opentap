71e092ca4a5394bde55ba6579f37e7daa8284a51
Frederik Andreasen <frederik.andreasen@keysight.com>
2022-06-09 09:30:28 +0200

    i505-Templates create project template Create template project and a project template.

---
+++ templates/src/project/proj/ProjectName.cs
@@ -0,0 +1,35 @@
+using OpenTap;
+
+namespace ProjectName
+{
+    [Display("ProjectName")]
+    public class ProjectName : TestStep
+    {
+        #region Settings
+        // ToDo: Add property here for each parameter the end user should be able to change
+        #endregion
+        public ProjectName()
+        {
+            // ToDo: Set default values for properties / settings.
+        }
+
+        public override void PrePlanRun()
+        {
+            base.PrePlanRun();
+            // ToDo: Optionally add any setup code this step needs to run before the testplan starts
+        }
+
+        public override void Run()
+        {
+            // ToDo: Add test case code here
+            RunChildSteps(); //If step has child steps.
+            UpgradeVerdict(Verdict.Pass);
+        }
+
+        public override void PostPlanRun()
+        {
+            // ToDo: Optionally add any cleanup code this step needs to run after the entire testplan has finished
+            base.PostPlanRun();
+        }
+    }
+}
