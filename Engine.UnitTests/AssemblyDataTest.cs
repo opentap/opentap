94d69e8b21bbcba7ee0411a89b9c8658dc00cf38
allarsen <alexander.larsen@keysight.com>
2022-06-27 14:52:11 +0200

    Add unittest to show the issue

--- Engine.UnitTests/AssemblyDataTest.cs
+++ Engine.UnitTests/AssemblyDataTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using NUnit.Framework;
 
 namespace OpenTap.UnitTests
@@ -37,6 +38,22 @@ namespace OpenTap.UnitTests
             assemblyDataVersionTest2("0.3.0.0", new SemanticVersion(0, 3, 0, null, null), "0.3.0.0");
             assemblyDataVersionTest2("1.0", true, "1.0");
             assemblyDataVersionTest2("asd", false, null);
+        }
+
+        [Test]
+        public void ConflictingAssemblyVersionTest()
+        {
+            var s = PluginManager.GetSearcher();
+            var globAsm = s.Assemblies.FirstOrDefault(a => a.Name.Contains("DotNet.Glob"));
+            
+            Assert.AreEqual(SemanticVersion.Parse("3.0.1"),  SemanticVersion.Parse(globAsm.SemanticVersion.ToString(3)));
+            Assert.AreEqual(Version.Parse("3.0.1.0"), globAsm.Version);
+            
+            var newtonsoftAsm = s.Assemblies.FirstOrDefault(a => a.Name.Contains("Newtonsoft.Json"));
+            Assert.AreEqual(SemanticVersion.Parse("12.0.3"), SemanticVersion.Parse(newtonsoftAsm.SemanticVersion.ToString(3)));
+            // The newtonsoft package is actually version 12.0.0.3, but the assembly version is 12.0.0.0 for some reason.
+            // This was changed in 9.18.2 due to a regression, but it has been so for a long long time.  
+            Assert.AreEqual(Version.Parse("12.0.0.0"), newtonsoftAsm.Version);
         }
 
         [Test]
