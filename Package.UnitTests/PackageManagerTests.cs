84fb47f9dc5a13631e0a59e6552b64ca4afb6496
Denrask <dennis.rasmussen@keysight.com>
2022-07-07 13:05:57 +0200

    Remove legacy HttpPackageRepository redirect test

--- Package.UnitTests/PackageManagerTests.cs
+++ Package.UnitTests/PackageManagerTests.cs
@@ -148,23 +148,6 @@ namespace OpenTap.Package.UnitTests
             manager.DownloadPackage(package, path, new System.Threading.CancellationToken());
             File.Delete(path);
         }
-
-        [Test]
-        public static void TestHttpRepositoryManagerRedirect()
-        {
-            (string initial, string redirected, string redirected_alt)[] urls = new[] {
-                ($"opentap.io", "https://www.opentap.io/", "https://opentap.io/"), // Redirect to www and https
-                ($"https://opentap.io", "https://opentap.io/", null), // no redirect
-                ($"packages.opentap.io", "http://packages.opentap.io/", null), // No redirect
-                ($"http://opentap.io", "https://www.opentap.io/", "https://opentap.io/")}; // Redirect to https
-
-            foreach (var url in urls)
-            {
-                var manager = new HttpPackageRepository(url.initial);
-                var redir = new Uri(manager.Url).AbsoluteUri;
-                Assert.IsTrue(Equals(redir, url.redirected) || (Equals(redir, url.redirected_alt) && url.redirected_alt != null));
-            }
-        }
     }
 
 
