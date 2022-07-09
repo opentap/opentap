bea0797996cbf80f3a452c8b8ea60279e9f6a487
Rolf Madsen <rolf_madsen@keysight.com>
2022-07-09 15:35:36 +0200

    460 Dependency Resolver exact: fixed sort order.

--- Package.UnitTests/Image/DependencyGraph.cs
+++ Package.UnitTests/Image/DependencyGraph.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using OpenTap.Package;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -43,64 +44,92 @@ namespace OpenTap.Image.Tests
 
     public class DependencyResolverGraphTest
     {
-        [Test]
         public void Test1()
         {
             var graph = PackageDependencyQuery.QueryGraph("https://packages.opentap.io").Result;
         }
 
-        static IEnumerable<(ImageSpecifier spec, PackageSpecifier[] result)> Specifiers()
+        static PackageSpecifier[] str2packages(string csv)
         {
-            {
-                var img = new ImageSpecifier();
-                img.Packages.Add("OpenTAP", "9.17.4");
-                img.Packages.Add("SDK", "any");
-                img.Packages.Add("Developer's System", "any");
-                img.Packages.Add("REST-API", "^2.6.0");
-                img.Packages.Add("Keysight Floating Licensing", "^1.4");
-                yield return (img, null);
-            }
-            {
-                var img = new ImageSpecifier();
-                img.Packages.Add("OpenTAP", "9.17.4");
-                img.Packages.Add("OSIntegration", "^1");
-                yield return (img, null);
-            }
-            {
-                var img = new ImageSpecifier();
-                img.Packages.Add("OpenTAP", "9.17.4");
-                img.Packages.Add("OpenTAP", "9.17.2");
-                yield return (img, null);
-            }
-            {
-                var img = new ImageSpecifier();
-                img.Packages.Add("OpenTAP", "9.17.4");
-                img.Packages.Add("Developer's System CE", "any");
-                yield return (img, null);
-            }
-            {
-                var img = new ImageSpecifier();
-                img.Packages.Add("OpenTAP", "9.17.4");
-                img.Packages.Add("REST-API", "any");
-                yield return (img, null);
-            }
-
+           return csv.Split(new char[]{';'}, StringSplitOptions.RemoveEmptyEntries).Select(x => x.Split(','))
+                .Select(x => new PackageSpecifier(x[0].Trim(), VersionSpecifier.Parse(x[1].Trim())))
+                .ToArray();
         }
         
-        [Test]
-        public void ParsePackages()
+        static ImageSpecifier image(string csv)
+        {
+            var packages = str2packages(csv);
+            var image = new ImageSpecifier();
+            image.Packages.AddRange(packages);
+            return image;
+        }
+        
+        static string unresolvable = nameof(unresolvable);
+        static IEnumerable<(string spec, string result)> Specifiers
+        {
+        get{
+            yield return (
+                "OpenTAP, 9.17.4; SDK, any; Developer's System, any; REST-API, ^2.6.0; Keysight Floating Licensing, ^1.4",
+                "CSV, 9.11.0+98498e58;Developer's System, 9.17.4+5e508f08;Editor, 9.17.4+5e508f08;" +
+                "Keysight Floating Licensing, 1.4.1+e5816333;Keysight Licensing, 1.1.1+7a2a1fe3;" +
+                "OpenTAP, 9.17.4;OSIntegration, 1.4.2+15f32a31;REST-API, 2.6.0+895d94fa;" +
+                "Results Viewer, 9.17.4+5e508f08;SDK, 9.17.4+ea67c63a;SQLite and PostgreSQL, 9.4.2+3009aace;" +
+                "Timing Analyzer, 9.17.4+5e508f08;Visual Studio SDK, 1.2.6+ef0ad804;WPF Controls, 9.17.4+5e508f08");
+            yield return ("OpenTAP, 9.17.4; OSIntegration, ^1", "OpenTAP, 9.17.4;OSIntegration, 1.4.2+15f32a31");
+            yield return ("OpenTAP, 9.17.4; OpenTAP, 9.17.2", null);
+            yield return ("OpenTAP, 9.17.4;Developer's System CE, any", "CSV, 9.11.0+98498e58;Developer's System CE, 9.17.4+5e508f08;Editor CE, 9.17.4+5e508f08;" +
+                                                                        "Keysight Licensing, 1.1.1+7a2a1fe3;OpenTAP, 9.17.4;OSIntegration, 1.4.2+15f32a31;" +
+                                                                        "Results Viewer CE, 9.17.4+5e508f08;SDK, 9.17.4+ea67c63a;SQLite and PostgreSQL, 9.4.2+3009aace;" +
+                                                                        "Visual Studio SDK CE, 1.2.6+ef0ad804;WPF Controls, 9.17.4+5e508f08");
+            yield return ("OpenTAP, 9.17.4; REST-API, any", "Keysight Floating Licensing, 1.4.1+e5816333;Keysight Licensing, 1.0.0+8a228623;" +
+                                                            "OpenTAP, 9.17.4;REST-API, 2.7.0+2d67ea81");
+            yield return ("OpenTAP, 9.17; OSIntegration, ^1", "OpenTAP, 9.17.4;OSIntegration, 1.4.2+15f32a31");
+
+        }}
+
+        static IEnumerable<string[]> Specifiers2
+        {
+            get
+            {
+                foreach (var elem in Specifiers)
+                {
+                    yield return new string[] { elem.spec, elem.result };
+                }
+            }
+        }
+
+        readonly PackageDependencyGraph graph;
+        public DependencyResolverGraphTest()
         {
             var packagesResourceName = "OpenTap.Package.UnitTests.Image.opentap-packages.json.gz";
-            var graph = PackageDependencyQuery.LoadGraph(Assembly.GetExecutingAssembly()
+            graph = PackageDependencyQuery.LoadGraph(Assembly.GetExecutingAssembly()
                 .GetManifestResourceStream(packagesResourceName), true);
+        }
+        
+        [TestCaseSource(nameof(Specifiers2))]
+        public void ParsePackages(string spec, string result)
+        {
+            var resolver2 = new ImageResolver();
+            var sw = Stopwatch.StartNew();
+            var img = image(spec);
+            var r = resolver2.ResolveImage(img, graph);
 
-            foreach (var spec in Specifiers())
+
+            if (result == null)
+                Assert.IsNull(r);
+            else
             {
-                var resolver2 = new ImageResolver();
-                var sw = Stopwatch.StartNew();
-                var result = resolver2.ResolveImage(spec.spec, graph);
-                var elapsed = sw.Elapsed;
+                Assert.IsNotNull(r);
+                var imageString = string.Join(";", r.Packages.Select(x => $"{x.Name}, {x.Version}"));
+                if (result == null) return;
+                foreach (var pkg in str2packages(result))
+                {
+                    Assert.IsTrue(r.Packages.Any(x => x.Name == pkg.Name && x.Version.Equals(pkg.Version)));
+                }
             }
+
+            var elapsed = sw.Elapsed;
+            
         }
     }
 }
