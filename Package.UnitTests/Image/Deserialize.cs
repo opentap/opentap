d2f21aaa90851a7af87f7050a8851e0edd4d9c2f
Denrask <dennis.rasmussen@keysight.com>
2022-07-12 20:41:23 +0200

    Fix TestJsonImage unittest

--- Package.UnitTests/Image/Deserialize.cs
+++ Package.UnitTests/Image/Deserialize.cs
@@ -56,7 +56,7 @@ namespace OpenTap.Image.Tests
             Assert.True(specifier.Packages[1].OS == "Windows");
 
             Assert.True(specifier.Repositories.Count == 3);
-            Assert.True(specifier.Repositories[0] == PackageCacheHelper.PackageCacheDirectory);
+            Assert.True(specifier.Repositories[0] == new Uri(PackageCacheHelper.PackageCacheDirectory).AbsoluteUri);
             Assert.True(specifier.Repositories[1] == "packages.opentap.io");
             Assert.True(specifier.Repositories[2] == "packages.opentap.keysight.com");
 
