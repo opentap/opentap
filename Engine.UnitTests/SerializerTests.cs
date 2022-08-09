2dc2bea5aec7cc1a714492cd027591df8ec9966d
Alexander Larsen <alexander.larsen@keysight.com>
2022-08-09 09:28:32 +0200

    Add unittest to show the issue

--- Engine.UnitTests/SerializerTests.cs
+++ Engine.UnitTests/SerializerTests.cs
@@ -1,5 +1,9 @@
+using System;
+using System.Collections.Generic;
 using System.Xml.Linq;
 using NUnit.Framework;
+using OpenTap.Package;
+using OpenTap.Plugins.BasicSteps;
 
 namespace OpenTap.Engine.UnitTests
 {
@@ -23,6 +27,43 @@ namespace OpenTap.Engine.UnitTests
             Assert.AreEqual("X Setting", xdoc.Root.Element("X").Attribute("Metadata").Value);
             Assert.AreEqual("Y", xdoc.Root.Element("Y").Attribute("Metadata").Value);
         }
-    
+
+        [Test]
+        public void TestPackageDependencySerializer()
+        {
+            var plan = new TestPlan()
+            {
+                ChildTestSteps = { new DelayStep() }
+            };
+
+            var ser = new TapSerializer();
+            { // verify that a serialized plan has package dependencies
+                var str = ser.SerializeToString(plan);   
+                CollectionAssert.IsEmpty(ser.Errors);
+                var elem = XElement.Parse(str);
+                Assert.AreEqual(1, elem.Elements("Package.Dependencies").Count());
+            }
+            { // verify that a serialized collection of plans has no package dependencies
+                var plans = new TestPlan[]
+                {
+                    plan,
+                    plan
+                };
+                var str = ser.SerializeToString(plans);
+                CollectionAssert.IsEmpty(ser.Errors);
+                var elem = XElement.Parse(str);
+                Assert.AreEqual(0, elem.Elements("Package.Dependencies").Count());            
+            }
+            { // verify that a serialized list of package versions does not have package dependencies
+                var versions = new PackageVersion[]
+                {
+                    new PackageVersion("pkg", SemanticVersion.Parse("1.0.0"), "Linux", CpuArchitecture.AnyCPU, DateTime.Now, new List<string>())
+                };
+                var str = ser.SerializeToString(versions);
+                CollectionAssert.IsEmpty(ser.Errors);
+                var elem = XElement.Parse(str);
+                Assert.AreEqual(0, elem.Elements("Package.Dependencies").Count());  
+            }
+        }
     }
 }
