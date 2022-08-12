e211c81b378b61ffec350489f67bc8ef595bfde5
Alexander Larsen <alexander.larsen@keysight.com>
2022-08-12 15:15:03 +0200

    Refactor the PackageVersion deserializer to use a default serializer when possible

--- Package/PackageVersionSerializerPlugin.cs
+++ Package/PackageVersionSerializerPlugin.cs
@@ -11,21 +11,64 @@ namespace OpenTap.Package
 
         public override bool Deserialize(XElement node, ITypeData t, Action<object> setter)
         {
-            if (t.IsA(typeof(PackageVersion[])))
-            {
-                var list = new List<PackageVersion>();
-                foreach (var element in node.Elements())
-                    list.Add(DeserializePackageVersion(element));
+            if (t.IsA(typeof(PackageVersion)) == false) return false;
 
-                setter(list.ToArray());
-                return true;
-            }
-            if (t.IsA(typeof(PackageVersion)))
+            var version = new PackageVersion();
+
+            void setProp(string propertyName, XObject obj)
             {
-                setter(DeserializePackageVersion(node));
-                return true;
+                // CPU was removed in OpenTAP 9.0. This is to support packages created by TAP 8x
+                if (propertyName == "CPU") 
+                    propertyName = "Architecture";
+
+                var prop = typeof(PackageVersion).GetProperty(propertyName);
+                if (prop == null) return;
+                
+                // Deserialize using a default serializer when possible
+                if (obj is XElement elem)
+                {
+                    Serializer.Deserialize(elem, o => prop.SetValue(version, o), prop.PropertyType);
+                    return;
+                }
+
+                // Obj is always either an XElement or an XAttribute
+                var value = (obj as XAttribute)!.Value;
+                if (prop.PropertyType.IsEnum)
+                    prop.SetValue(version, Enum.Parse(prop.PropertyType, value));
+                else if (prop.PropertyType == typeof(SemanticVersion))
+                {
+                    if (SemanticVersion.TryParse(value, out var semver))
+                        prop.SetValue(version, semver);
+                    else
+                        Log.Warning($"Cannot parse version '{value}' of package '{version.Name ?? "Unknown"}'.");
+                }
+                else if (prop.PropertyType == typeof(DateTime))
+                {
+                    if (DateTime.TryParse(value, out var date))
+                        prop.SetValue(version, date);
+                }
+                else
+                {
+                    prop.SetValue(version, value);
+                }
             }
-            return false;
+
+            var elements = node.Elements().ToList();
+            var attributes = node.Attributes().ToList();
+
+            foreach (var element in elements)
+            {
+                if (element.IsEmpty) continue;
+                setProp(element.Name.LocalName, element);
+            }
+
+            foreach (var attribute in attributes)
+            {
+                setProp(attribute.Name.LocalName, attribute);
+            }
+
+            setter(version);
+            return true;
         }
 
         public override bool Serialize(XElement node, object obj, ITypeData expectedType)
@@ -59,57 +102,6 @@ namespace OpenTap.Package
             
             // The serialization of this element should be handled by a generic serializer
             return false;
-        }
-
-        PackageVersion DeserializePackageVersion(XElement node)
-        {
-            var version = new PackageVersion();
-            var elements = node.Elements().ToList();
-            var attributes = node.Attributes().ToList();
-            foreach (var element in elements)
-            {
-                if (element.IsEmpty) continue;
-                setProp(element.Name.LocalName, element.Value);
-            }
-            foreach (var attribute in attributes)
-            {
-                setProp(attribute.Name.LocalName, attribute.Value);
-            }
-
-            return version;
-
-            void setProp(string propertyName, string value)
-            {
-                if (propertyName == "CPU") // CPU was removed in OpenTAP 9.0. This is to support packages created by TAP 8x
-                    propertyName = "Architecture";
-
-                var prop = typeof(PackageVersion).GetProperty(propertyName);
-                if (prop == null) return;
-                if (prop.PropertyType.IsEnum)
-                    prop.SetValue(version, Enum.Parse(prop.PropertyType, value));
-                else if (prop.PropertyType.HasInterface<IList<string>>())
-                {
-                    var list = new List<string>();
-                    list.Add(value);
-                    prop.SetValue(version, list);
-                }
-                else if (prop.PropertyType == typeof(SemanticVersion))
-                {
-                    if (SemanticVersion.TryParse(value, out var semver))
-                        prop.SetValue(version, semver);
-                    else
-                        Log.Warning($"Cannot parse version '{value}' of package '{version.Name ?? "Unknown"}'.");
-                }
-                else if (prop.PropertyType == typeof(DateTime))
-                {
-                    if (DateTime.TryParse(value, out var date))
-                        prop.SetValue(version, date);
-                }
-                else
-                {
-                    prop.SetValue(version, value);
-                }
-            }
         }
     }
 }
