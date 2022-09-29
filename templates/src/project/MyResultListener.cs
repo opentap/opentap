a3aea8dd4182d1ea30b98741d81bbcb5657a5671
Frederik Andreasen <frederik.andreasen@keysight.com>
2022-09-29 13:23:35 +0200

    Change template names to be MyXX

--- templates/src/project/MyResultListener.cs
+++ templates/src/project/MyResultListener.cs
@@ -7,13 +7,13 @@ using OpenTap;
 
 namespace ProjectName
 {
-    [Display("ProjectNameResultListener")]
-    public class ProjectNameResultListener : ResultListener
+    [Display("MyResultListener")]
+    public class MyResultListener : ResultListener
     {
         #region Settings
         // ToDo: Add property here for each parameter the end user should be able to change
         #endregion
-        public ProjectNameResultListener()
+        public MyResultListener()
         {
             // ToDo: Set default values for properties / settings.
             Name = "ProjectNameResultListener";
