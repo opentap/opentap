9630042b4b0b6468beacec61b5234dfa3c6209eb
Rolf Madsen <rolf_madsen@keysight.com>
2023-01-23 23:22:00 +0100

    970 Search Async Memory Leak: Fix
    - Fixed the issue by creating a cache class which can be cleared when PluginManager.ChangeID changes.
    - Added unit test for the cache class.
    - Added Cache class which is a key-value lookup datastructure which can be cleared based on the result of some calculation.

---
+++ Shared/Cache.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Immutable;
+namespace OpenTap
+{
+    /// <summary> Cache class which clears when a certain criteria is met. </summary>
+    internal class Cache<K, V>
+    {
+        // using immutable dictionary to avoid issues with race conditions.
+        // note that for caches data races are not that important.
+        ImmutableDictionary<K, V> dict = ImmutableDictionary<K, V>.Empty;
+        
+        // this function returns an object which is used for checking if the cache should be cleared.
+        readonly Func<object> clearTrigger;
+        
+        // last object returned by clearTrigger.
+        object currentObj;
+
+        ///<summary> The current number of cached elements. </summary> 
+        public int Count
+        {
+            get
+            {
+                CheckClear();
+                return dict.Count;
+            }
+        }
+
+        public Cache(Func<object> clearTrigger)
+        {
+            this.clearTrigger = clearTrigger;
+            currentObj = clearTrigger();
+        }
+        
+        void CheckClear()
+        {
+            var clearCheck = clearTrigger();
+            if (Equals(clearCheck, currentObj) == false)
+            {
+                dict = ImmutableDictionary<K, V>.Empty;
+                currentObj = clearTrigger();
+            }
+        }
+
+        /// <summary> tries getting a value for a key. </summary>
+        public bool TryGetValue(K key, out V value)
+        {
+            CheckClear();
+            return dict.TryGetValue(key, out value);
+        }
+
+        public V AddValue(K key, V value)
+        {
+            dict = dict.Add(key, value);
+            return value;
+        }
+    }
+}
