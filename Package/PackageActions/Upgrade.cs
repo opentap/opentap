d3d4ecb310f9e251ca866b7fe22f0ef4e556dcb5
Alexander Larsen <alexander.larsen@keysight.com>
2023-02-16 17:10:38 +0100

    Add upgrade cli action

---
+++ Package/PackageActions/Upgrade.cs
@@ -0,0 +1,81 @@
+﻿using System.ComponentModel;
+using System.Linq;
+using System.Threading;
+using OpenTap.Cli;
+
+namespace OpenTap.Package
+{
+    [Display("upgrade", Group: "package")]
+    internal class Upgrade : IsolatedPackageAction
+    {
+        [CommandLineArgument("repository", Description = CommandLineArgumentRepositoryDescription, ShortName = "r")]
+        public string[] Repository { get; set; }
+
+
+        protected override int LockedExecute(CancellationToken cancellationToken)
+        {
+            var repositories = PackageManagerSettings.Current.GetEnabledRepositories(Repository);
+            var updates = UpdateCheck.GetUpdates(cancellationToken, repositories);
+
+            if (!updates.Any())
+            {
+                log.Info("Nothing to do.");
+                return 0;
+            }
+
+            var installedPackages = Installation.Current.GetPackages().ToArray();
+            var image = new ImageSpecifier();
+            foreach (var update in updates.GroupBy(p => p.Name))
+            {
+                var updatedPackage = update.FindMax(p => p.Version);
+                if (updatedPackage == null) continue;
+                image.Packages.Add(updatedPackage.GetSpecifier());
+            }
+            
+            image.Repositories.AddRange(repositories.Select(r => r.Url));
+            
+            var resolved = image.MergeAndResolve(new Installation(Target), cancellationToken);
+
+            log.Info("The following updates will be installed:");
+            foreach (var update in resolved.Packages)
+            {
+                var currentPackage = installedPackages.FirstOrDefault(p => p.Name == update.Name);
+                if (currentPackage == null)
+                {
+                    log.Info($" - {update.Name}: {update.Version} (New package)");
+                    continue;
+                }
+                if (currentPackage.Version == update.Version) continue;
+                log.Info($" - {update.Name}: {currentPackage.Version} -> {update.Version}");
+            }
+
+            var question = new ContinueRequest();
+            UserInput.Request(question);
+            if (question.Response == ContinueResponse.Cancel)
+                return 0;           
+            
+            resolved.Deploy(Target, cancellationToken);
+
+            return 0;
+        }
+        
+        enum ContinueResponse
+        {
+            Continue,
+            Cancel,
+        }
+
+        class ContinueRequest
+        {
+            [Browsable(true)]
+            [Layout(LayoutMode.FullRow)]
+            public string Message => message;
+            internal string message;
+            public string Name { get; private set; } = "Continue?";
+
+            [Submit]
+            [Layout(LayoutMode.FullRow | LayoutMode.FloatBottom)]
+            public ContinueResponse Response { get; set; }
+        }
+    }
+}
--- Package/UpdateCheck.cs
+++ Package/UpdateCheck.cs
@@ -25,26 +25,13 @@ namespace OpenTap.Package
 
         static readonly TraceSource Log = OpenTap.Log.CreateSource("UpdateCheck");
 
-        private void CheckForUpdatesAsync(CancellationToken cancellationToken)
+        internal static List<PackageDef> GetUpdates(CancellationToken cancellationToken,
+            List<IPackageRepository> repos)
         {
-            string noUpdateCheckEnv = Environment.GetEnvironmentVariable("OPENTAP_NO_UPDATE_CHECK");
-            bool noUpdateCheck = noUpdateCheckEnv == "true" || noUpdateCheckEnv == "1";
-
-            if (Startup && (PackageManagerSettings.Current.CheckForUpdates == false || noUpdateCheck))
-                return;
-
-            // Since we are deciding to do the update check for the parent process there is no reason to also
-            // do it for the child processes.
-            Environment.SetEnvironmentVariable("OPENTAP_NO_UPDATE_CHECK", "true");
-            string noUpdateMessageEnv = Environment.GetEnvironmentVariable("OPENTAP_NO_UPDATE_MESSAGE");
-            bool noUpdateMessage = noUpdateMessageEnv == "true" || noUpdateMessageEnv == "1";
-
-            var timer = Stopwatch.StartNew();
-            List<PackageDef> updates = new List<PackageDef>();
-
-            var installation = new Installation(ExecutorClient.ExeDir);
+            var updates = new List<PackageDef>();
+            var installation = Installation.Current;
             IPackageIdentifier[] installedPackages = installation.GetPackages().ToArray();
-            Parallel.ForEach(PackageManagerSettings.Current.GetEnabledRepositories(null), repo =>
+            Parallel.ForEach(repos, repo =>
             {
                 try
                 {
@@ -60,9 +47,30 @@ namespace OpenTap.Package
                 }
             });
 
-            if (noUpdateMessage)
+            return updates;
+        }
+        
+        private static string noUpdateCheckEnv = Environment.GetEnvironmentVariable("OPENTAP_NO_UPDATE_CHECK");
+        private static string noUpdateMessageEnv = Environment.GetEnvironmentVariable("OPENTAP_NO_UPDATE_MESSAGE");
+        private static bool noUpdateMessage = noUpdateMessageEnv == "true" || noUpdateMessageEnv == "1";
+
+
+        private void CheckForUpdatesAsync(CancellationToken cancellationToken)
+        {
+            bool noUpdateCheck = noUpdateCheckEnv == "true" || noUpdateCheckEnv == "1";
+
+            if (Startup && (PackageManagerSettings.Current.CheckForUpdates == false || noUpdateCheck))
                 return;
 
+            // Since we are deciding to do the update check for the parent process there is no reason to also
+            // do it for the child processes.
+            Environment.SetEnvironmentVariable("OPENTAP_NO_UPDATE_CHECK", "true");
+
+            var timer = Stopwatch.StartNew();
+            List<PackageDef> updates = GetUpdates(cancellationToken, PackageManagerSettings.Current.GetEnabledRepositories(null));
+            if (noUpdateMessage) return;
+            
+            IPackageIdentifier[] installedPackages = Installation.Current.GetPackages().ToArray();
 
             if (updates.Any())
             {
@@ -73,6 +81,7 @@ namespace OpenTap.Package
                     var updatedPackage = update.OrderByDescending(p => p.Version).FirstOrDefault();
                     Log.Info($" - {update.Key}: {currentPackage?.Version} -> {updatedPackage?.Version}");
                 }
+                Log.Info("To update all packages, run 'tap package update'");
             }
             else
             {
