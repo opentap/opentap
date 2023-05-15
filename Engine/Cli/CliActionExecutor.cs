b3377a9a207cc14ae028e6d6aef82e5bea3bf859
Alexander Larsen <alexander.larsen@keysight.com>
2023-05-15 13:17:41 +0200

    Acquire lock during execute

--- Engine/Cli/CliActionExecutor.cs
+++ Engine/Cli/CliActionExecutor.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.IO;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
+using System.Threading;
 using System.Xml.Linq;
 
 namespace OpenTap.Cli
@@ -300,6 +301,39 @@ namespace OpenTap.Cli
                     return (int)ExitCodes.Success;
                 else
                     return (int)ExitCodes.ArgumentParseError;
+            }
+
+            { // Check if the lock for the current installation can be acquired
+                var target = ExecutorClient.ExeDir;
+                var lockfile = Path.Combine(target, ".lock");
+                FileSystemHelper.EnsureDirectoryOf(lockfile);
+                const int limit = 30;
+                bool taken = false;
+                
+                for (int i = 0; i < limit; i++)
+                {
+                    using var fileLock = FileLock.Create(lockfile);
+                    try
+                    {
+                        taken = fileLock.WaitOne(TimeSpan.FromSeconds(1));
+                    }
+                    catch (AbandonedMutexException)
+                    {
+                        // ignore -- this happens if the mutex is disposed in another process.
+                        // In this case we will most likely acquire the lock in the next iteration.
+                        continue;
+                    }
+
+                    // the lock is released during dispose
+                    if (taken) break;
+                    log.Info(
+                        $"{target} is locked by a locking package action. Waiting for it to become unlocked... ({i + 1} / {limit})");
+                }
+
+                if (!taken)
+                {
+                    throw new Exception($"Unable to acquire a lock on the installation.");
+                }
             }
 
             if (SelectedAction != TypeData.FromType(typeof(RunCliAction)) && UserInput.Interface == null) // RunCliAction has --non-interactive flag and custom platform interaction handling.          
--- Shared/FileLocks.cs
+++ Shared/FileLocks.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Runtime.InteropServices;
 using System.Threading;
 
-namespace OpenTap.Package.PackageInstallHelpers
+namespace OpenTap
 {
     internal interface IFileLock : IDisposable
     {
