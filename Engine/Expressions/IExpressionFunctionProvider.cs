295d21ff9146b3aed86d9cdf748aa873b69694cd
Rolf Madsen <rolf_madsen@keysight.com>
2023-06-12 16:55:10 +0200

    945 User Defined Step Settings: methods
    - added support for methods that can be called.
    - methods can be added with a plugin.

---
+++ Engine/Expressions/IExpressionFunctionProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Reflection;
+namespace OpenTap.Expressions
+{
+    public interface IExpressionFunctionProvider
+    {
+        MethodInfo GetMethod(string name, Type[] argumentTypes);
+        string[] GetCompletions(string name);
+    }
+
+}
