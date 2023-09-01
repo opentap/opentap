257d5183fa12893c245203ff6a1fad19319bc857
Rolf Madsen <rolf_madsen@keysight.com>
2023-09-01 09:57:58 +0200

    1223 OpenTap Package Refrerence User Token: set-auto-token -> tap login
    - Renamed "tap package set-auth-token" to "tap login". --token has to be used for now, but we allow the future to implement some more user-friendly features.

--- Engine/Cli/LoginAction.cs
+++ Engine/Cli/LoginAction.cs
@@ -1,42 +1,39 @@
 using System;
-using System.ComponentModel;
-using System.Linq;
 using System.Threading;
 using OpenTap.Authentication;
 using OpenTap.Cli;
-namespace OpenTap.Package
+namespace OpenTap
 {
     /// <summary> Sets the authentication token for use with a given domain.  </summary>
-    [Display("set-auth-token", Group: "package", Description: "Sets the authentication token for a given domain.")]
-    public class SetAuthToken : ICliAction
+    [Display("login",Description: "Log in to a online service. Only supports setting the token directly using '--token'.")]
+    public class LoginAction : ICliAction
     {
         /// <summary>  Gets or sets the domain. </summary>
         [UnnamedCommandLineArgument("url")]
+        [Display("url")]
+        
         public string Url { get; set; }
 
         /// <summary>  Gets or sets the token. </summary>
-        [UnnamedCommandLineArgument("token")]
+        [CommandLineArgument("token")]
         public string Token { get; set; }
+       
+        static readonly TraceSource log = Log.CreateSource("login");
         
-        /// <summary>  Gets or sets update - if existing settings should be updated. </summary>
-        [CommandLineArgument("update", ShortName = "u", Description = "Sets if existing settings should be updated.")]
-        public bool Update { get; set; }
-
-        static readonly TraceSource log = Log.CreateSource("set-auth-token");
-        
+        /// <summary> Logs in to the url. </summary>
         public int Execute(CancellationToken cancellationToken)
         {
+            // We currently only support by 'logging in' by setting the token directly
+            // using --token. In the future we can add support for letting the server tell us how to 
+            // log in through the browser.
+            if (string.IsNullOrEmpty(Token))
+            {
+                log.Error("Currently only --token is a supported method of login");
+                return -1;
+            }
             var uri = new Uri(Url, UriKind.RelativeOrAbsolute);
             var authority = uri.IsAbsoluteUri ? uri.Authority : Url;
             log.Debug($"Selecting {authority} as authority.");
-            if (AuthenticationSettings.Current.Tokens.Any(x => x.Domain == authority))
-            {
-                if (!Update)
-                {
-                    log.Error($"Settings already contains a token for {authority}. Use --update to overwrite it.");
-                    return -1;
-                }
-            }
             
             AuthenticationSettings.Current.Tokens.RemoveIf(x => x.Domain == authority);
             AuthenticationSettings.Current.Tokens.Add(new TokenInfo(Token, "", authority));
