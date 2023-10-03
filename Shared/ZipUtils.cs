5490f99b85f410d877549cf94dd877e43d1a1747
Alexander Larsen <alexander.larsen@keysight.com>
2023-10-03 16:53:18 +0200

    Add reflection workaround when packaging on platforms other than windows

---
+++ Shared/ZipUtils.cs
@@ -0,0 +1,25 @@
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.Reflection;
+
+namespace OpenTap
+{
+    static class ZipUtils
+    {
+        private static PropertyInfo _externalAttributes = null;
+        public static void SetPermissions(this ZipArchiveEntry entry, string permissions)
+        {
+            // This API is only available on .NET 6 and above -- access it via reflection if possible
+            // entry.ExternalAttributes = entry.ExternalAttributes | (Convert.ToInt32("664", 8) << 16);
+
+            if (OperatingSystem.Current != OperatingSystem.Windows)
+            {
+                _externalAttributes ??= typeof(ZipArchiveEntry).GetProperty("ExternalAttributes"); 
+                var flags = Convert.ToInt32(permissions, 8) << 16;
+                var current = (int)_externalAttributes.GetValue(entry);
+                _externalAttributes.SetValue(entry, current | flags);
+            }
+        }
+    }
+}
