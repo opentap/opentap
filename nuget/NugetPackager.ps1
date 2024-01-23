175f7f28172b32ad2a3bcf551e04e54233ac6434
Alexander Larsen <alexander.larsen@keysight.com>
2024-01-23 16:02:44 +0100

    Refactor powershell packaging script

--- nuget/NugetPackager.ps1
+++ nuget/NugetPackager.ps1
@@ -23,90 +23,39 @@ function CopyPreserveRelativePath{
     Copy-Item $filename $newItem
 }
 
-# Copy linux arm64 specific files
-Push-Location linux-arm64
+$platforms=@( "linux-arm64", "linux-x64", "macos-arm64", "macos-x64" )
+foreach ( $platform in $platforms )
+{
+  Push-Location $platform
 
-$runtimeDir = "../nuget/build/runtimes/linux-arm64"
-$packageXml = Get-ChildItem -File -Recurse package.xml | Resolve-Path -Relative | Select-Object -First 1
-CopyPreserveRelativePath $packageXml $runtimeDir
+  $runtimeDir = "../nuget/build/runtimes/$platform"
+  $packageXml = Get-ChildItem -File -Recurse package.xml | Resolve-Path -Relative | Select-Object -First 1
+  CopyPreserveRelativePath $packageXml $runtimeDir
 
-CopyPreserveRelativePath ./tap $runtimeDir
-CopyPreserveRelativePath ./tap.dll $runtimeDir
-CopyPreserveRelativePath ./tap.runtimeconfig.json $runtimeDir
-Get-ChildItem -File -Recurse libgit2-* | Resolve-Path -Relative | ForEach-Object { CopyPreserveRelativePath $_ $runtimeDir }
+  CopyPreserveRelativePath ./tap $runtimeDir
+  CopyPreserveRelativePath ./tap.dll $runtimeDir
+  CopyPreserveRelativePath ./tap.runtimeconfig.json $runtimeDir
+  Get-ChildItem -File -Recurse libgit2-* | Resolve-Path -Relative | ForEach-Object { CopyPreserveRelativePath $_ $runtimeDir }
 
-Pop-Location
+  Pop-Location
+}
 
-# Copy linux x64 specific files
-Push-Location linux-x64
+$win_arch=@( "x86", "x64" )
+foreach ( $arch in $win_arch )
+{
+  Push-Location "win-$arch"
 
-$runtimeDir = "../nuget/build/runtimes/linux-x64"
-$packageXml = Get-ChildItem -File -Recurse package.xml | Resolve-Path -Relative | Select-Object -First 1
-CopyPreserveRelativePath $packageXml $runtimeDir
+  $runtimeDir = "../nuget/build/runtimes/win-$arch"
+  $packageXml = Get-ChildItem -File -Recurse package.xml | Resolve-Path -Relative | Select-Object -First 1
+  CopyPreserveRelativePath $packageXml $runtimeDir
 
-CopyPreserveRelativePath ./tap $runtimeDir
-CopyPreserveRelativePath ./tap.dll $runtimeDir
-CopyPreserveRelativePath ./tap.runtimeconfig.json $runtimeDir
-Get-ChildItem -File -Recurse libgit2-* | Resolve-Path -Relative | ForEach-Object { CopyPreserveRelativePath $_ $runtimeDir }
+  CopyPreserveRelativePath ./tap.exe  $runtimeDir
+  # .net462 revert:
+  #CopyPreserveRelativePath ./tap.dll $runtimeDir
+  #CopyPreserveRelativePath ./tap.runtimeconfig.json $runtimeDir
 
-Pop-Location
+  $git2dll = Get-ChildItem -File -Recurse "*git2-*.dll.$arch" | Resolve-Path -Relative | Select-Object -First 1
+  CopyPreserveRelativePath $git2dll $runtimeDir
 
-# Copy macos x64 specific files
-Push-Location macos-x64
-
-$runtimeDir = "../nuget/build/runtimes/macos-x64"
-$packageXml = Get-ChildItem -File -Recurse package.xml | Resolve-Path -Relative | Select-Object -First 1
-CopyPreserveRelativePath $packageXml $runtimeDir
-
-CopyPreserveRelativePath ./tap $runtimeDir
-CopyPreserveRelativePath ./tap.dll $runtimeDir
-CopyPreserveRelativePath ./tap.runtimeconfig.json $runtimeDir
-Get-ChildItem -File -Recurse libgit2-* | Resolve-Path -Relative | ForEach-Object { CopyPreserveRelativePath $_ $runtimeDir }
-
-Pop-Location
-
-# Copy macos arm64 specific files
-Push-Location macos-arm64
-
-$runtimeDir = "../nuget/build/runtimes/macos-arm64"
-$packageXml = Get-ChildItem -File -Recurse package.xml | Resolve-Path -Relative | Select-Object -First 1
-CopyPreserveRelativePath $packageXml $runtimeDir
-
-CopyPreserveRelativePath ./tap $runtimeDir
-CopyPreserveRelativePath ./tap.dll $runtimeDir
-CopyPreserveRelativePath ./tap.runtimeconfig.json $runtimeDir
-Get-ChildItem -File -Recurse libgit2-* | Resolve-Path -Relative | ForEach-Object { CopyPreserveRelativePath $_ $runtimeDir }
-
-Pop-Location
-
-# Copy win-x86 specific files
-Push-Location win-x86
-$runtimeDir = "../nuget/build/runtimes/win-x86"
-$packageXml = Get-ChildItem -File -Recurse package.xml | Resolve-Path -Relative | Select-Object -First 1
-CopyPreserveRelativePath $packageXml $runtimeDir
-
-CopyPreserveRelativePath ./tap.exe  $runtimeDir
-# .net462 revert:
-#CopyPreserveRelativePath ./tap.dll $runtimeDir
-#CopyPreserveRelativePath ./tap.runtimeconfig.json $runtimeDir
-
-$git2dll = Get-ChildItem -File -Recurse *git2-*.dll.x86 | Resolve-Path -Relative | Select-Object -First 1
-CopyPreserveRelativePath $git2dll $runtimeDir
-
-Pop-Location
-
-# Copy win-x64 specific files
-Push-Location win-x64
-$runtimeDir = "../nuget/build/runtimes/win-x64"
-$packageXml = Get-ChildItem -File -Recurse package.xml | Resolve-Path -Relative | Select-Object -First 1
-CopyPreserveRelativePath $packageXml $runtimeDir
-
-CopyPreserveRelativePath ./tap.exe  $runtimeDir
-# .net462 revert:
-#CopyPreserveRelativePath ./tap.dll $runtimeDir
-#CopyPreserveRelativePath ./tap.runtimeconfig.json $runtimeDir
-
-$git2dll = Get-ChildItem -File -Recurse *git2-*.dll.x64 | Resolve-Path -Relative | Select-Object -First 1
-CopyPreserveRelativePath $git2dll $runtimeDir
-
-Pop-Location
+  Pop-Location
+}
