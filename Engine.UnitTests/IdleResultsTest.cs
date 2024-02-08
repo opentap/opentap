cae546e7c693612d8bf187f26fb7b8f96ee08609
Rolf Madsen <rolf_madsen@keysight.com>
2024-02-08 11:29:55 +0100

    Metrics incremental work and testing.

--- Engine.UnitTests/IdleResultsTest.cs
+++ Engine.UnitTests/IdleResultsTest.cs
@@ -1,16 +1,52 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Linq;
 using System.Text;
 using NUnit.Framework;
 
 namespace OpenTap.UnitTests
 {
+    public class TestMetricProducer : IMetricProducer
+    {
+        [Metric] [Unit("I")] public double X { get; private set; }
+
+        [Metric]
+        [Unit("V")]
+        public double Y { get; private set; }
+
+        private int _offset = 0;
+        public void PushMetric()
+        {
+            var xMetric = Metrics.GetMetricInfo(this, nameof(X));
+            var yMetric = Metrics.GetMetricInfo(this, nameof(Y));
+            if (!Metrics.MetricHasInterest(xMetric)) return;
+            var x = new List<double>();
+            var y = new List<double>();
+            for (int i = 0; i < 100; i++)
+            {
+                _offset += 1;
+                X = _offset;
+                
+                x.Add(_offset);
+                y.Add(Y = Math.Sin(_offset * 0.1));
+            }
+
+            var table = new ResultTable("TestMetricProducer", new ResultColumn[]
+            {
+                new ResultColumn("X", x.ToArray()),
+                new ResultColumn("Y", y.ToArray())
+            });
+            Metrics.PushMetric(table, xMetric, yMetric);
+
+        }
+        
+    }
+    
     [TestFixture]
     public class IdleResultsTest
-    {
-        
-        
+    {   
         public class IdleResultTestInstrument : Instrument, IMetricUpdateCallback, IStatusName
         {
             public IdleResultTestInstrument()
@@ -33,7 +69,7 @@ namespace OpenTap.UnitTests
             [Display("I", Group: "Metrics")]
             [Metric]
             public double Current { get; private set; }
-
+            
             public void UpdateMetrics()
             {
                 Voltage = Math.Sin(sw.Elapsed.TotalSeconds * 100.0) + 2.5;
@@ -45,6 +81,25 @@ namespace OpenTap.UnitTests
                 UpdateMetrics();
                 Metrics.NotifyMetricsChanged(this);
             }
+            
+            [Metric]
+            [Unit("cm")]
+            public int I { get; private set; }
+
+            public void PushRangeValues()
+            {
+                var iMetric = Metrics.GetMetricInfo(this, nameof(I));
+                if (Metrics.MetricHasInterest(iMetric) == false)
+                    return;
+                var lst = new List<int>();
+                for (int i = 0; i < 10; i++)
+                {
+                    lst.Add(i);
+                    I++;
+                }
+                Metrics.PushMetric(new ResultTable(Name, new []{iMetric.CreateColumn(lst.ToArray())}), iMetric);
+            }
+            
         }
 
         public class TestMetricsConsumer : IMetricConsumer
@@ -53,6 +108,38 @@ namespace OpenTap.UnitTests
             {
                 
             }
+            StringBuilder sb = new StringBuilder();
+
+            public string GetString() => sb.ToString();
+
+            public void Clear()
+            {
+                sb.Clear();
+            }
+            
+            
+            public void OnPushMetric(ResultTable table)
+            {
+                
+                sb.Append("=========== ");
+                sb.Append(table.Name);
+                sb.AppendLine(" =========== ");
+                foreach (var column in table.Columns)
+                {
+                    string unit = "";
+                    unit = column.Parameters["Unit"]?.ToString() ?? "";
+                    sb.AppendFormat($"{column.Name} : {string.Join( ", ", column.Data.Cast<object>())} {unit}");
+                    sb.AppendLine();
+                }
+
+
+                var text = sb.ToString();
+
+            }
+
+            public HashSet<MetricInfo> MetricFilter { get; set; } = new HashSet<MetricInfo>();
+
+            public IEnumerable<MetricInfo> GetInterest(IEnumerable<MetricInfo> allMetrics) => allMetrics.Where(x => x.Member.Name != "I");
         }
 
         [Test]
@@ -60,13 +147,32 @@ namespace OpenTap.UnitTests
         {
             using (Session.Create())
             {
-                var consumer = new TestMetricsConsumer(); 
-                Metrics.RegisterMetricConsumer(consumer);
                 InstrumentSettings.Current.Clear();
+                var consumer = new TestMetricsConsumer(); 
                 var instrTest = new IdleResultTestInstrument();
                 
                 InstrumentSettings.Current.Add(instrTest);
-                var metrics = Metrics.GetMetrics();
+
+                foreach (var metric in Metrics.GetMetricInfos())
+                {
+                    consumer.MetricFilter.Add(metric);
+                }
+                
+                Metrics.RegisterMetricConsumer(consumer);
+                Metrics.PollMetrics();
+                
+
+                instrTest.InvokeMetricsDate();
+                instrTest.PushRangeValues();
+
+                var str = consumer.GetString();
+                consumer.Clear();
+                consumer.MetricFilter.Remove(consumer.MetricFilter.FirstOrDefault(x => x.Member.Name == "I"));
+                Metrics.PollMetrics();
+                instrTest.PushRangeValues();
+                var str2 = consumer.GetString();
+                Assert.AreNotEqual(str, str2);
+                /*var metrics = Metrics.GetMetrics();
                 instrTest.InvokeMetricsDate();
                 StringBuilder sb = new StringBuilder();
                 foreach (var table in metrics)
@@ -85,7 +191,13 @@ namespace OpenTap.UnitTests
 
                 var text = sb.ToString();
 
+                var producerMetrics = Metrics.GetMetrics((obj) => obj.Source is TestMetricProducer);
+                Assert.AreEqual(1, producerMetrics.Length);*/
+
             }
         }
     }
+
+
+    
 }
--- Engine/Metrics.cs
+++ Engine/Metrics.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -13,6 +14,89 @@ namespace OpenTap
         void UpdateMetrics();
     }
 
+    /// <summary>
+    /// Information about a given metric,
+    /// </summary>
+    public class MetricInfo 
+    {
+        /// <summary>
+        /// The source object of the metric.
+        /// </summary>
+        public readonly object Source;
+        /// <summary>
+        /// The metric member objectr.
+        /// </summary>
+        public IMemberData Member;
+        /// <summary>
+        /// The name of the metric group.
+        /// </summary>
+        public string MetricName { get; }
+
+        /// <summary>
+        /// creates an instance.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="mem"></param>
+        /// <param name="memberKey"></param>
+        public MetricInfo(object source, IMemberData mem, string memberKey)
+        {
+            Source = source;
+            Member = mem;
+            MetricName = memberKey;
+        }
+
+        /// <summary>
+        /// Provides name for the metric.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString() => $"{MetricName}: {Member.Name}";
+
+        /// <summary>
+        /// Creates a column.
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public ResultColumn CreateColumn(Array array)
+        {
+            var list = new List<IParameter>();
+            if(Member.GetAttribute<UnitAttribute>() is UnitAttribute unit)
+                list.Add(new ResultParameter("Unit", unit.Unit));
+            
+            return new ResultColumn(this.Member.GetDisplayAttribute().Name, array, list.ToArray());
+        }
+
+        /// <summary>
+        /// Implements equality for metric info.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            if (obj is MetricInfo otherMetric)
+            {
+                return otherMetric.MetricName == MetricName && object.Equals(otherMetric.Member, Member) &&
+                       Source == otherMetric.Source;
+            }
+
+            return false;
+        }
+        
+        /// <summary>
+        /// Hash code for metrics.
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = (Source != null ? Source.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (Member != null ? Member.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (MetricName != null ? MetricName.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
+    }
+    
     /// <summary> Defines that a class can consume metrics. </summary>
     public interface IMetricConsumer
     {
@@ -21,8 +105,23 @@ namespace OpenTap
         /// </summary>
         /// <param name="subject"></param>
         void OnMetricsUpdate(object subject);
+
+        /// <summary>  Event occuring when a metric producer generates out-of-band metrics. </summary>
+        void OnPushMetric(ResultTable table);
+
+        /// <summary>
+        /// Defines which in a list of metrics are the ones that have interest.
+        /// </summary>
+        /// <param name="allMetrics"></param>
+        /// <returns></returns>
+        IEnumerable<MetricInfo> GetInterest(IEnumerable<MetricInfo> allMetrics);
     }
 
+    /// <summary> Marker interface for classes that produce metrics. </summary>
+    public interface IMetricProducer
+    {
+    }
+    
     /// <summary> Gives a short name based on metrics. </summary>
     public interface IStatusName
     {
@@ -46,11 +145,9 @@ namespace OpenTap
         /// <summary> Creates a new instance of the metric attribute.</summary>
         public MetricAttribute() : this(null)
         {
-            
         }
-        
     }
-    
+
     /// <summary>
     /// Utility methods for metrics,.
     /// </summary>
@@ -58,14 +155,14 @@ namespace OpenTap
     {
         private static WeakHashSet<IMetricConsumer> consumers =
             new WeakHashSet<IMetricConsumer>();
-        
+
         /// <summary> Register a metric consumer. </summary>
         /// <param name="consumer"></param>
         public static void RegisterMetricConsumer(IMetricConsumer consumer)
         {
             consumers.Add(consumer);
         }
-        
+
         /// <summary>
         /// Notifies that a specific object with metrics has been updated.
         /// </summary>
@@ -78,39 +175,87 @@ namespace OpenTap
             }
         }
 
-        /// <summary> Gets metrics from an object. </summary>
-        public static ResultTable[] GetMetrics(object subject)
+        /// <summary>
+        /// Returns true if a metric has interest.
+        /// </summary>
+        /// <param name="metric"></param>
+        /// <returns></returns>
+        public static bool MetricHasInterest(MetricInfo metric)
         {
-            return GetMetrics((obj) => obj == subject);
+            return interest.Contains(metric);
         }
         
-        /// <summary>  Gets all metrics in the system.  </summary>
-        public static ResultTable[] GetMetrics(Func<object, bool> filter = null)
+        /// <summary> Get information about the metrics available to query. </summary>
+        /// <returns></returns>
+        public static IEnumerable<MetricInfo> GetMetricInfos()
         {
-            if (filter == null) filter = (obj) => true;
-            List<ResultTable> results = new List<ResultTable>();
-            foreach (IResource instr in InstrumentSettings.Current.Cast<IResource>().Concat(DutSettings.Current).Where(filter))
+            var types = TypeData.GetDerivedTypes<IMetricProducer>().Where(x => x.CanCreateInstance);
+            List<IMetricProducer> producers = new List<IMetricProducer>();
+            foreach (var type in types.Select(t => t))
             {
-                if (instr is IMetricUpdateCallback metricUpdate)
+                if (type.DescendsTo(typeof(ComponentSettings)))
                 {
-                    metricUpdate.UpdateMetrics();
+                    var item2 = ComponentSettings.GetCurrent(type) as IMetricProducer;
+                    if(item2 != null)
+                        producers.Add(item2);
                 }
+                else
+                {
+                    if (metricProducers.GetOrAdd(type, t => (IMetricProducer)t.CreateInstance()) is IMetricProducer m)
+                        producers.Add(m);
+                }
+            }
 
+            foreach (object instr in InstrumentSettings.Current.Cast<object>().Concat(DutSettings.Current)
+                         .Concat(producers))
+            {
+            
                 var td = TypeData.GetTypeData(instr);
+                string name = (instr as IResource)?.Name ?? td.GetDisplayAttribute().Name;
                 var memberGrp = td.GetMembers()
                     .Where(member => member.HasAttribute<MetricAttribute>())
-                    .ToLookup(td => td.GetAttribute<MetricAttribute>().Name ?? instr.Name);
+                    .ToLookup(td => td.GetAttribute<MetricAttribute>().Name ?? name);
                 foreach (var member in memberGrp)
                 {
-                    var columns = member.Select(mem =>
+                    foreach(var mem in member)
+                        yield return new MetricInfo(instr, mem, member.Key);
+                }
+            }
+        }
+
+        private static readonly ConcurrentDictionary<ITypeData, IMetricProducer> metricProducers =
+            new ConcurrentDictionary<ITypeData, IMetricProducer>();
+
+        /// <summary>  Gets all metrics in the system.  </summary>
+        public static ResultColumn[] GetMetrics(Func<MetricInfo, bool> filter = null)
+        {
+            if (filter == null) filter = (info) => true;
+            var providers = GetMetricInfos().Where(filter).ToArray();
+            foreach (var cb in providers.Select(p => p.Source).OfType<IMetricUpdateCallback>())
+            {
+                cb.UpdateMetrics();
+            }
+
+            var results = new List<ResultColumn>();
+
+            foreach (var metricBySource in providers.ToLookup(metric => metric.Source))
+            {
+                var obj = metricBySource.Key;
+
+                var td = TypeData.GetTypeData(metricBySource.Key);
+                string name = (obj as IResource)?.Name ?? td.GetDisplayAttribute()?.Name;
+                var metricsBySource = metricBySource.ToLookup(x => x.MetricName);
+                foreach (var sourceGroup in metricsBySource)
+                {
+                    var columns = sourceGroup.Select(mem =>
                     {
                         var parameters = new List<ResultParameter>();
-                        if (mem.GetAttribute<UnitAttribute>() is UnitAttribute unit)
+                        if (mem.Member.GetAttribute<UnitAttribute>() is UnitAttribute unit)
                         {
                             parameters.Add(new ResultParameter("Unit", unit.Unit));
                         }
 
-                        var memValue = mem.GetValue(instr) ?? "";
+                        var memValue = mem.Member.GetValue(obj) ?? "";
                         Array array;
                         if (memValue is IConvertible)
                         {
@@ -121,18 +266,111 @@ namespace OpenTap
                             array = new string[1];
                             memValue = memValue.ToString();
                         }
-                        
+
                         array.SetValue(memValue, 0);
-                        
-                        return new ResultColumn(mem.GetDisplayAttribute().Name, array, parameters.ToArray());
+
+                        return new ResultColumn(mem.Member.GetDisplayAttribute().Name, array, parameters.ToArray());
                     });
-                    
-                    var table = new ResultTable(td.GetDisplayAttribute().Name, columns.ToArray());
-                    results.Add(table);
+
+                    results.AddRange(columns);
                 }
             }
-
             return results.ToArray();
+        }
+
+        private static MetricInfo[] allMetrics;
+        private static HashSet<MetricInfo> interest = new HashSet<MetricInfo>();
+        
+        /// <summary>
+        /// Push a metric,
+        /// </summary>
+        /// <param name="table"></param>
+        /// <param name="columnMetrics"></param>
+        /// <exception cref="ArgumentException"></exception>
+        public static void PushMetric(ResultTable table, params MetricInfo[] columnMetrics)
+        {
+            if (table.Columns.Length != columnMetrics.Length)
+                throw new ArgumentException("There has to be as many metrics as columns");
+            foreach (var consumer in consumers.GetElements())
+            {
+                var thisInterest = consumer.GetInterest(columnMetrics);
+                if(thisInterest.Any())
+                    consumer.OnPushMetric(table);
+            }
+        }
+
+        /// <summary>
+        /// Poll metrics.
+        /// </summary>
+        public static void PollMetrics()
+        {
+            if (allMetrics == null)
+                allMetrics = GetMetricInfos().ToArray();
+            Dictionary<IMetricConsumer, MetricInfo[]> interestLookup = new Dictionary<IMetricConsumer, MetricInfo[]>();
+            foreach (var consumer in consumers.GetElements())
+            {
+                interestLookup[consumer] = consumer.GetInterest(allMetrics).ToArray();
+            }
+
+            var interest2 = interestLookup.Values.SelectMany(x => x).Distinct().ToHashSet();
+            interest = interest2;
+            foreach (var producer in interest2.Select(x => x.Source).Distinct().OfType<IMetricUpdateCallback>())
+            {
+                producer.UpdateMetrics();
+            }
+            Dictionary<MetricInfo, object> metricValues = new Dictionary<MetricInfo, object>();
+            foreach (var metric in interest2)
+            {
+                metricValues[metric] = metric.Member.GetValue(metric.Source);
+            }
+
+
+
+            foreach (var consumer in interestLookup)
+            {
+                foreach (var grp in consumer.Value.GroupBy(x => x.MetricName))
+                {
+                    var columns = grp.Select(mem =>
+                    {
+                        var parameters = new List<ResultParameter>();
+                        if (mem.Member.GetAttribute<UnitAttribute>() is UnitAttribute unit)
+                        {
+                            parameters.Add(new ResultParameter("Unit", unit.Unit));
+                        }
+
+                        var memValue = metricValues[mem] ?? "";
+                        Array array;
+                        if (memValue is IConvertible)
+                        {
+                            array = Array.CreateInstance(memValue.GetType(), 1);
+                        }
+                        else
+                        {
+                            array = new string[1];
+                            memValue = memValue.ToString();
+                        }
+
+                        array.SetValue(memValue, 0);
+
+                        return new ResultColumn(mem.Member.GetDisplayAttribute().Name, array, parameters.ToArray());
+                    });
+                    var table = new ResultTable(grp.Key, columns.ToArray());
+                    consumer.Key.OnPushMetric(table);
+                }
+            }
+        }
+         
+        /// <summary>
+        /// Get metric information from the system.
+        /// </summary>
+        /// <param name="p0"></param>
+        /// <param name="yName"></param>
+        /// <returns></returns>
+        public static MetricInfo GetMetricInfo(object p0, string yName)
+        {
+            if(allMetrics == null)
+                PollMetrics();
+            return allMetrics?.FirstOrDefault(metric => metric.Source == p0 && metric.Member.Name == yName);
         }
     }
 }
