966b627f9f207fc5101fdf736ea68b4cd328a0a8
Alexander Larsen <alexander.larsen@keysight.com>
2024-03-07 14:23:18 +0100

    659 Add IStringValueAnnotation implementation for DateTime (#660)
    * 659 Add IStringValueAnnotation implementation for DateTime
    
    This adds a IStringValueAnnotation implementation for DateTime. We already have one for TimeSpan, so I think it makes sense to add one for DateTime as well.
    
    * using date time format "yyyy-MM-dd HH:mm:ss"
    
    To make the date time format compliant with the normally used one.
    
    ---------
    
    Co-authored-by: Rolf Madsen <rolf_madsen@keysight.com>

--- Engine/Annotations/Annotation.cs
+++ Engine/Annotations/Annotation.cs
@@ -139,7 +139,7 @@ namespace OpenTap
         string Example { get; }
     }
 
-    /// <summary> Defines how an error annotation works. Note: Multiple of IErrorAnnotation can be used in the same annotation. In this case the erros will be concatenated. </summary>
+    /// <summary> Defines how an error annotation works. Note: Multiple of IErrorAnnotation can be used in the same annotation. In this case the errors will be concatenated. </summary>
     public interface IErrorAnnotation : IAnnotation
     {
         /// <summary> The list of errors for this annotation. </summary>
@@ -2785,6 +2785,8 @@ namespace OpenTap
 
                 if (mem.ReflectionInfo.DescendsTo(typeof(TimeSpan)))
                     annotation.Add(new TimeSpanAnnotation(annotation));
+                if (mem.ReflectionInfo.DescendsTo(typeof(DateTime)))
+                    annotation.Add(new DateTimeAnnotation(annotation));
                 
                 Sequence.ProcessPattern(attributes, 
                     (UnitAttribute x) => annotation.Add(x), 
