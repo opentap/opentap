1cc50e620525a24865303981bf02688c7d66619a
Alexander Larsen <alexander.larsen@keysight.com>
2024-04-03 15:24:27 +0200

    Fix issue causing incorrect exceptions to be set when thrown in Defer

--- Engine/TestStep.cs
+++ Engine/TestStep.cs
@@ -1030,13 +1030,12 @@ namespace OpenTap
                     {
                         runTask.Wait();
                     }
-                    catch (TestStepBreakException e)
-                    {
-                        TestPlan.Log.Info(e.Message);
-                        Step.Verdict = Verdict.Error;
-                    }
                     catch (Exception e)
                     {
+                        // Tasks wrap exceptions in AggregateExceptions with a single exception
+                        while (e is AggregateException aex && aex.InnerExceptions.Count == 1)
+                            e = aex.InnerException;
+                        
                         if (e is ThreadAbortException || (e is OperationCanceledException && TapThread.Current.AbortToken.IsCancellationRequested) )
                         {
                             if (TapThread.Current.AbortToken.IsCancellationRequested && Step.Verdict < Verdict.Aborted)
@@ -1045,6 +1044,11 @@ namespace OpenTap
                                 TestPlan.Log.Warning("Test step {0} was canceled.", stepRun.TestStepPath);
                             else
                                 TestPlan.Log.Warning("Test step {0} was canceled with message '{1}'.", stepRun.TestStepPath, e.Message);
+                        }
+                        else if (e is TestStepBreakException brk)
+                        {
+                            TestPlan.Log.Info(brk.Message);
+                            Step.UpgradeVerdict(brk.Verdict);
                         }
                         else
                         {
