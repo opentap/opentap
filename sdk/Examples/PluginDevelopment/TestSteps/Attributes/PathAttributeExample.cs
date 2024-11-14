45c7a2d624cd7a478c9efa1699aebf19fa741652
Rolf Madsen <rolf_madsen@keysight.com>
2024-11-14 10:11:43 +0100

    1827 Fixed SDK examples build error

--- sdk/Examples/PluginDevelopment/TestSteps/Attributes/PathAttributeExample.cs
+++ sdk/Examples/PluginDevelopment/TestSteps/Attributes/PathAttributeExample.cs
@@ -12,6 +12,7 @@
 //See the License for the specific language governing permissions and
 //limitations under the License.
 
+using System.Collections.Generic;
 namespace OpenTap.Plugins.PluginDevelopment
 {
     [Display("Path Attributes Example", Groups: new[] {"Examples", "Plugin Development", "Attributes" },
