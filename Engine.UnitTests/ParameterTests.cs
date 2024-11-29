07a9ab2f6bd4e846be327d9855393b63ad8e53ff
Rolf Madsen <rolf_madsen@keysight.com>
2024-11-29 16:15:06 +0100

    1846 Multi Level Scope bug: Test

---
+++ Engine.UnitTests/ParameterTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using OpenTap.Plugins.BasicSteps;
+namespace OpenTap.UnitTests;
+
+[TestFixture]
+public class ParameterTests
+{
+    [Test]
+    public void TestMultiLevelParameters()
+    {
+        // copying multi-level parameters has been a problem because the parameter got removed
+        // the moment it was no longer a part of the test plan, even if the parameter was inside the scopes.
+
+        var seq3 = new SequenceStep();
+        var seq = new SequenceStep();
+        var seq2 = new SequenceStep();
+        var delay = new DelayStep();
+        seq.ChildTestSteps.Add(seq2);
+        seq2.ChildTestSteps.Add(delay);
+        seq3.ChildTestSteps.Add(seq);
+
+        TypeData.GetTypeData(delay).GetMember(nameof(delay.DelaySecs)).Parameterize(seq, delay, "A");
+
+        Assert.IsTrue(TypeData.GetTypeData(seq).GetMember("A") != null);
+
+    }
+    
+}
