da04c17f76bdb6de527f7819142e6ed754db71bf
Alexander Larsen <alexander.larsen@keysight.com>
2025-08-25 10:55:41 +0200

    Update all projects to use GenerateAssemblyInfo

--- BasicSteps/Properties/AssemblyInfo.cs
+++ BasicSteps/Properties/AssemblyInfo.cs
@@ -8,8 +8,6 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OpenTap.Plugins.BasicSteps")]
-[assembly: AssemblyDescription("Basic Test Steps like loop and if.")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
--- Engine.UnitTests/Properties/AssemblyInfo.cs
+++ Engine.UnitTests/Properties/AssemblyInfo.cs
@@ -8,8 +8,6 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OpenTap.UnitTests")]
-[assembly: AssemblyDescription("Unit Tests for the OpenTAP Engine")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
