556ad06dde2007c4a37296762eadd7d0d6c86275
Rolf Madsen <rolf_madsen@keysight.com>
2025-09-08 14:04:24 +0200

    2105 Custom Step Names: Fix
    Added an interface that enables a test step to do custom name formatting.
    
    Added unit tests to verify the feature.

---
+++ Engine.UnitTests/TestStepNameFormattingTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+namespace OpenTap.UnitTests;
+
+public class TestStepNameFormattingTests
+{
+    class FormatFormattedNameStep : OpenTap.TestStep, IFormatName
+    {
+        public string ReplaceWith { get; set; } = "123";
+        public FormatFormattedNameStep()
+        {
+            Name = "__CUSTOM__";
+        }
+        public override void Run()
+        {
+
+        }
+
+        public string GetFormattedName()
+        {
+            return Name.Replace("__CUSTOM__", ReplaceWith);
+        }
+    }
+
+    [TestCase("<__CUSTOM__>", "123", "<123>")]
+    [TestCase("__CUSTOM____CUSTOM__", "A", "AA")]
+    [TestCase("55555", "123123", "55555")]
+    public void TestICustomNameFormatter(string name, string replace, string expected)
+    {
+        var fmt = new FormatFormattedNameStep()
+        {
+            Name = name,
+            ReplaceWith = replace
+        };
+        Assert.AreEqual(expected, fmt.GetFormattedName());
+    }
+
+}
