437a263b5a25114b0ddc782983ff06fcbffab201
Alexander Larsen <alexander.larsen@keysight.com>
2026-08-14 10:40:22 +0200

    update copyright years (#2449)
    Updating copyright years.

--- sdk/Examples/PluginDevelopment/Advanced Examples/MenuModelExample.cs
+++ sdk/Examples/PluginDevelopment/Advanced Examples/MenuModelExample.cs
@@ -1,4 +1,4 @@
-﻿//Copyright 2012-2021 Keysight Technologies
+﻿//Copyright 2012-2026 Keysight Technologies
 //
 //Licensed under the Apache License, Version 2.0 (the "License");
 //you may not use this file except in compliance with the License.
